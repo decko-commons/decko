e556474e22b138714dcbfe4ce52f54b1be57085b
xithan <philipp.kuehl@gmail.com>
2017-08-31 22:04:32 +0200

    add comment

--- card/config/initializers/02_patches/action_dispatch.rb
+++ card/config/initializers/02_patches/action_dispatch.rb
@@ -1,5 +1,7 @@
 module Patches
   class ActionDispatch
+    # needed to make rails-dev-tweaks gem to work with rails 5
+    # based on this PR https://github.com/wavii/rails-dev-tweaks/pull/25/files
     class Reloader
       class << self
         def cleanup!
