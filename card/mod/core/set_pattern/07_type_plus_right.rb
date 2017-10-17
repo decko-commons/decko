b45567cab134d3236ac5757ebe73646b8bf309d0
Ethan McCutchen <ethan@grasscommons.org>
2017-10-17 16:09:23 -0400

    fixed fetch recursions

--- card/mod/core/set_pattern/07_type_plus_right.rb
+++ card/mod/core/set_pattern/07_type_plus_right.rb
@@ -16,7 +16,7 @@ def prototype_args anchor
 end
 
 def anchor_name card
-  left = Card.quick_fetch card.name.left
+  left = Card.fetch card.name.left, new: {}
   type_name = left&.type_name || Card.default_type_id.cardname
   "#{type_name}+#{card.name.tag}"
 end
