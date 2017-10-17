04a478d69a243afa333fc6cdf61401b1109eb026
Ethan McCutchen <ethan@grasscommons.org>
2017-10-17 15:36:01 -0400

    content API upgrading

--- card/mod/core/set_pattern/07_type_plus_right.rb
+++ card/mod/core/set_pattern/07_type_plus_right.rb
@@ -16,8 +16,8 @@ def prototype_args anchor
 end
 
 def anchor_name card
-  left = card.left new: {}
-  type_name = (left && left.type_name) || Card[Card.default_type_id].name
+  left = Card.quick_fetch card.name.left
+  type_name = left&.type_name || Card.default_type_id.cardname
   "#{type_name}+#{card.name.tag}"
 end
 
