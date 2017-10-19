6e63bf470bb94164b5d32dd48e30fee711f2ff31
xithan <philipp.kuehl@gmail.com>
2017-10-19 19:00:28 +0200

    Fix select2 code file cards

--- card/mod/bootstrap/set/self/script_select2.rb
+++ card/mod/bootstrap/set/self/script_select2.rb
@@ -3,3 +3,8 @@ include_set Abstract::CodeFile
 def source_files
   "vendor/select2/dist/js/select2.js"
 end
+
+
+def source_dir
+  ""
+end
--- card/mod/bootstrap/set/self/style_select2.rb
+++ card/mod/bootstrap/set/self/style_select2.rb
@@ -3,3 +3,7 @@ include_set Abstract::CodeFile
 def source_files
   "vendor/select2/dist/css/select2.min.css"
 end
+
+def source_dir
+  ""
+end
