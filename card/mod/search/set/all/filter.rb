5c85de01c19a601a483cadb04751d2a13c1eeb8a
xithan <philipp.kuehl@gmail.com>
2017-10-31 14:48:10 +0100

    :dog2:

--- card/mod/search/set/all/filter.rb
+++ card/mod/search/set/all/filter.rb
@@ -2,7 +2,9 @@ format :html do
   # @param data [Hash] the filter categories. The hash needs for every category
   #   a hash with a label and a input_field entry.
   def filter_form data={}, sort_input_field=nil, form_args={}
-    render_haml :filter_form, categories: data, sort_input_field: sort_input_field, form_args: form_args
+    render_haml :filter_form, categories: data,
+                              sort_input_field: sort_input_field,
+                              form_args: form_args
   end
 
   def view_template_path view, set_path= __FILE__
