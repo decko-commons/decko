c06d379ba745b820c73350b9ad4e1954eb114a2a
Ethan McCutchen <ethan@grasscommons.org>
2017-11-06 12:38:47 -0500

    refactor email_template.rb

--- card/mod/notifications/set/type/email_template.rb
+++ card/mod/notifications/set/type/email_template.rb
@@ -10,73 +10,94 @@ rescue Net::SMTPError => exception
   errors.add :exception, exception.message
 end
 
-def process_html_message config, args, inline_attachment_url
-  process_message_field(
-    :html_message, config,
-    args.merge(inline_attachment_url: inline_attachment_url),
-    "email_html"
-  )
-  if config[:html_message].present?
-    config[:html_message] = Card::Mailer.layout config[:html_message]
-  end
-  config
-end
-
 format do
   view :mail, perms: :none, cache: :never do |args|
-    config = card.email_config(args)
-    text_message = config.delete(:text_message)
-    attachment_list = config.delete(:attach)
-    email_card = card # card is not accessible in the new_mail block
-    mail = Card::Mailer.new_mail(config) do
-      # inline attachments only work in the binding of this block
-      # so the current solution is to create a lambda here and pass it
-      # to the view where it is needed to create the image tag
-      # (see core view in Type::Image::EmailHtmlFormat)
-      inline_attachment_url =
-        lambda do |path|
-          attachments.inline[path] = ::File.read path
-          attachments[path].url
-        end
-      email_card.process_html_message config, args, inline_attachment_url
-      html_message = config.delete(:html_message)
-      alternative = (text_message.present? && html_message.present?)
+    config = card.email_config args
+    fmt = self # self is <Mail::Message> within the new_mail block
+    Card::Mailer.new_mail config do
+      attachment_list = config.delete :attach
+      fmt.message_body self, config, args, attachment_list
+      fmt.add_attachments self, attachment_list
+    end
+  end
 
-      if alternative
-        if attachment_list && !attachment_list.empty?
-          content_type "multipart/mixed"
-          part content_type: "multipart/alternative" do |copy|
-            copy.part content_type: "text/plain" do |plain|
-              plain.body = text_message
-            end
-            copy.part content_type: "text/html" do |html|
-              html.body = html_message
-            end
-          end
-        else
-          text_part { body text_message }
-          html_part do
-            content_type "text/html; charset=UTF-8"
-            body html_message
-          end
-        end
-      elsif html_message.present?
-        content_type "text/html; charset=UTF-8"
-        body html_message
-      else
-        content_type "text/plain; charset=UTF-8"
-        text_part { body text_message }
-      end
+  def message_body mail, config, args, attachment_list
+    text_message = config.delete :text_message
+    html_message = process_html_message mail, config, args
+    if text_message.present? && html_message.present?
+      text_and_html_message mail, text_message, html_message, attachment_list
+    elsif html_message.present?
+      html_body mail, html_message
+    else
+      text_body mail, text_message
+    end
+  end
 
-      if attachment_list
-        attachment_list.each_with_index do |cardname, i|
-          if (c = Card[cardname]) && c.respond_to?(:attachment)
-            add_file filename: "attachment-#{i + 1}.#{c.attachment.extension}",
-                     content: File.read(c.attachment.path)
-          end
-        end
+  def process_html_message mail, config, args
+    msg_args = args.merge inline_attachment_url: inline_attachment_lambda(mail)
+    card.process_message_field :html_message, config, msg_args, "email_html"
+    html_message_with_layout config.delete(:html_message)
+  end
+
+  def inline_attachment_lambda mail
+    # inline attachments require mail object. the current solution is to pass a block
+    # to the view where it is needed to create the image tag
+    # (see inline view in Type::Image::EmailHtmlFormat)
+    # it could make more sense to give the image direct access to the mail object?
+    lambda do |path|
+      mail.attachments.inline[path] = ::File.read path
+      mail.attachments[path].url
+    end
+  end
+
+  def html_message_with_layout msg
+    return unless msg.present?
+    Card::Mailer.layout msg
+  end
+
+  def text_and_html_message mail, text_message, html_message, attachment_list = nil
+    fmt = self
+    if attachment_list&.any?
+      mail.multipart_mixed text_message, html_message
+    else
+      mail.text_part { body text_message }
+      mail.html_part { fmt.html_body mail, html_message }
+    end
+  end
+
+  def multipart_mixed mail, text_message, html_message
+    mail.content_type "multipart/mixed"
+    mail.part content_type: "multipart/alternative" do |copy|
+      copy.part content_type: "text/plain" do |plain|
+        plain.body = text_message
       end
-    end  # TODO: add error handling
-    mail
+      copy.part content_type: "text/html" do |html|
+        html.body = html_message
+      end
+    end
+  end
+
+  def html_body mail, message
+    mail.content_type "text/html; charset=UTF-8"
+    mail.body message
+  end
+
+  def text_body mail, message
+    mail.content_type "text/plain; charset=UTF-8"
+    mail.text_part { body message }
+  end
+
+  def add_attachments mail, list
+    return unless list.present?
+    list.each_with_index do |cardname, i|
+      file_card = Card[cardname]
+      next unless file_card&.respond_to? :attachment
+      file = file_card.attachment
+      mail.add_file filename: attachment_name(file), content: File.read(file.path)
+    end
+  end
+
+  def attachment_name file
+    "attachment-#{i + 1}.#{file.extension}"
   end
 end
