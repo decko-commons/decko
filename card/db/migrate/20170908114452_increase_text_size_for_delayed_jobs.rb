0612667280f959fcafb1a22119c412acc733b9aa
xithan <philipp.kuehl@gmail.com>
2017-11-24 21:26:34 +0100

    theme migrations

--- card/db/migrate/20170908114452_increase_text_size_for_delayed_jobs.rb
+++ card/db/migrate/20170908114452_increase_text_size_for_delayed_jobs.rb
@@ -1,1 +1,9 @@
-increase text size for handler column in delayed jobs table
+class IncreaseTextSizeForDelayedJobs < ActiveRecord::Migration[5.1]
+  def self.up
+    change_column :delayed_jobs, :handler, :text, :limit => 16777215
+  end
+
+  def self.down
+    change_column :delayed_jobs, :handler, :text
+  end
+end
