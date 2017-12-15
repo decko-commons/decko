2c2ac21f92331aadee5e639c2a6d3ef9a41ab932
Ethan McCutchen <ethan@grasscommons.org>
2017-12-15 14:28:50 -0500

    :sunny:

--- card/mod/core/spec/set/all/fetch_helper_spec.rb
+++ card/mod/core/spec/set/all/fetch_helper_spec.rb
@@ -14,25 +14,25 @@ RSpec.describe Card::Set::All::FetchHelper do
       retrieve
     end
 
-    it "doesn't look in db for real cards already in cache" do
+    it "doesn't look in db for cached cards(real)" do
       Card.cache.write "a", Card["B"]
       expect(Card).not_to receive(:retrieve_from_db)
       retrieve
     end
 
-    it "doesn't look in db for new cards already in cache" do
+    it "doesn't look in db for cached cards (new)" do
       Card.cache.write "a", Card.new
       expect(Card).not_to receive(:retrieve_from_db)
       retrieve
     end
 
-    it "doesn't look in db for real cards already in cache if 'look_in_trash' option used" do
+    it "doesn't look in db for cached cards (real) if 'look_in_trash' option used" do
       Card.cache.write "a", Card["B"]
       expect(Card).not_to receive(:retrieve_from_db)
       retrieve_from_trash
     end
 
-    it "looks in db for new cards already in cache if 'look_in_trash' option used" do
+    it "looks in db for cached cards (new) if 'look_in_trash' option used" do
       Card.cache.write "a", Card.new
       expect(Card).to receive(:retrieve_from_db).with(:key, "a", true)
       retrieve_from_trash
