62d3a1c98f201b82a2022e23e34d35bd5820939a
Ethan McCutchen <ethan@grasscommons.org>
2018-01-26 14:54:46 -0500

    :sunny: (?)

--- decko/lib/decko/response.rb
+++ decko/lib/decko/response.rb
@@ -35,11 +35,7 @@ module Decko
     def update_params_for_success
       success = Card::Env.success
       Card::Env[:params] = self.params =
-          if success.soft_redirect?
-            success.params
-          else # need tests. insure we get slot, main...
-            params.merge success.params
-          end
+        success.soft_redirect? ? success.params : params.merge(success.params)
     end
 
     def send_deprecated_asset
