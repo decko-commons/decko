535ce2a8d030527854d3863fe78f889b4a46766a
Ethan McCutchen <ethan@grasscommons.org>
2018-04-11 14:35:11 -0400

    updated head testing

--- card/mod/standard/spec/set/self/head_spec.rb
+++ card/mod/standard/spec/set/self/head_spec.rb
@@ -1,16 +1,43 @@
 # -*- encoding : utf-8 -*-
 
 describe Card::Set::Self::Head do
-  it "has a javascript tag" do
-    assert_view_select render_card(:raw, name: "*head"),
-                       'script[type="text/javascript"]'
-  end
+  subject { render_card :core, name: "*head" }
 
-  context "tinyMCE config" do
-    before do
-      @tiny_mce = Card[:tiny_mce]
+  describe "head tag" do
+    it "has (a) meta tag(s)" do
+      is_expected.to have_tag(:meta, charset: "UTF-8")
     end
-    it "handles tinyMCE configuration errors" do
+
+    it "has a title" do
+      is_expected.to have_tag(:title, text: "*head - My Deck")
+    end
+
+    it "has a favicon" do
+      is_expected.to have_tag(:link, rel: "shortcut icon",
+                                     href: "/files/:favicon/standard-small.png")
+    end
+
+    it "has a main javascript tag" do
+      is_expected.to have_tag(:script,
+                              src: "/files/:all_script_machine_output/machines.js")
+    end
+
+    it "has a main stylesheets link" do
+      is_expected.to have_tag(:link,
+                              rel: "stylesheet", media: "all", type: "text/css",
+                              href: "/files/:all_script_machine_output/machines.js")
+    end
+
+    it "handles tinyMCE configuration" do
+      is_expected.to match(/decko\.setTinyMCEConfig/)
+    end
+
+    it "triggers slotReady" do
+      is_expected.to match(/trigger\W*slotReady/)
+    end
+
+    it "sets rootPath" do
+      is_expected.to match(/window\.decko\W+rootPath/)
     end
   end
 end
