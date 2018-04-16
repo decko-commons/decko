e6efe0601f0a9ec9c8e2ae7250c55f2230fd3738
xithan <philipp.kuehl@gmail.com>
2018-04-16 17:00:19 +0200

    seed tick

--- card/mod/bootstrap/set/self/script_select2.rb
+++ card/mod/bootstrap/set/self/script_select2.rb
@@ -1,6 +1,8 @@
 include_set Abstract::CodeFile
 
 def source_files
+  # full version of select2 is needed to support containerCssClass config option
+  # which we need for select2_bootstrap to work properly
   "vendor/select2/dist/js/select2.full.min.js"
 end
 
