9ce02ce3bc25738ba325eb9312b201a79bc9ffb9
xithan <philipp.kuehl@gmail.com>
2018-04-17 22:00:47 +0200

    pointer-select

--- card/mod/pointer/set/abstract/code_pointer.rb
+++ card/mod/pointer/set/abstract/code_pointer.rb
@@ -3,8 +3,7 @@ include_set Abstract::Pointer
 abstract_basket :item_codenames
 
 def content
-  @cached_content ||=
-    item_codenames.map do |codename|
-      Card.fetch_name codename
-    end.compact.to_pointer_content
+  item_codenames.map do |codename|
+    Card.fetch_name codename
+  end.compact.to_pointer_content
 end
