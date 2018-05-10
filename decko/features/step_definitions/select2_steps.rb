33b16f0420f372f47154dd045cbda0fa3208cb08
xithan <philipp.kuehl@gmail.com>
2018-05-10 16:11:20 +0200

    fix navbox feature

---
+++ decko/features/step_definitions/select2_steps.rb
@@ -0,0 +1,31 @@
+When(/^I enter "([^"]*)" in the navbox$/) do |arg|
+  enter_select2(arg, '.navbox-form')
+end
+
+When(/^I enter '([^']*)' in the navbox$/) do |arg|
+  enter_select2(arg, '.navbox-form')
+end
+
+When /^(?:|I )select2 "([^"]*)" from "([^"]*)"$/ do |value, field|
+  select_from_select2(value, from: field)
+end
+
+def select_from_select2(value, attrs)
+  find("[name='#{attrs[:from]}'] + .select2-container").click
+  list = find(:xpath, '//span[@class="select2-results"]', visible: :all)
+  list.find("li", text: value).click
+end
+
+
+def enter_select2 value, css
+  select2_container = find(:css, css)
+  sleep 0.1
+  @container = select2_container.find(".select2-selection, .select2-choices", visible: false)
+  @container.click
+  find(:xpath, "//body").find(".select2-search input.select2-search__field").set(value)
+end
+
+When /^I press enter to search$/ do
+  @container.native.send_keys(:return)
+  # find("#query_keyword").native.send_keys(:return)
+end
