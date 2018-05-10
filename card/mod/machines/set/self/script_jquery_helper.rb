96154c9464b0abb31eaf1f206d83e416e14d9ec6
xithan <philipp.kuehl@gmail.com>
2018-05-10 15:26:57 +0200

    add jquery file upload back

--- card/mod/machines/set/self/script_jquery_helper.rb
+++ card/mod/machines/set/self/script_jquery_helper.rb
@@ -4,8 +4,12 @@ Self::ScriptLibraries.add_item :script_jquery_helper
 def source_files
   # jquery.ui.all must be after jquery.mobile to override dialog weirdness *
   # FIXME removed  jquerymobile.js. Doesn't work with the new jquery version
-  # as fas as I'm aware of we use the only jquery widgets we use are
-  # autocomplete and autosize
+  # as fas as I'm aware of the only jquery widgets we use are
+  # autocomplete, autosize and fileupload
   # autocomplete is intergrated in jquery-ui
-  %w[jquery-ui.js jquery.autosize.js]
+  # don't know if iframe-transport is needed
+  %w[jquery-ui.js
+     jquery.autosize.js
+     ../../vendor/jquery_file_upload/js/jquery.fileupload.js
+     ../../vendor/jquery_file_upload/js/jquery.iframe-transport.js]
 end
