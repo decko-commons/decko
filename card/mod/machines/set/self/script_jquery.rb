bf1356a22a9c8a3667434706eef5ce3da4a3af6f
xithan <philipp.kuehl@gmail.com>
2018-05-12 15:07:37 +0200

    machine output update

--- card/mod/machines/set/self/script_jquery.rb
+++ card/mod/machines/set/self/script_jquery.rb
@@ -1,7 +1,6 @@
 include_set Abstract::CodeFile
 
 def source_files
-  #%w[lib/javascript/script_jquery.js lib/javascript/jquery_ujs.js]
   ["vendor/jquery_rails/vendor/assets/javascripts/jquery3.js",
    "vendor/jquery_rails/vendor/assets/javascripts/jquery_ujs.js"]
 end
