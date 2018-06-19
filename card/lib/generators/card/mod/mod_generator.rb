990cc3bba44c289b4fbc38acc06e8f9105128eab
xithan <philipp.kuehl@gmail.com>
2018-06-19 15:38:56 -0400

    :sunny:

--- card/lib/generators/card/mod/mod_generator.rb
+++ card/lib/generators/card/mod/mod_generator.rb
@@ -7,14 +7,18 @@ class Card
     class ModGenerator < NamedBase
       source_root File.expand_path("../templates", __FILE__)
 
-      class_option "core", type: :boolean, aliases: "-c",
+      class_option "core",
+                   type: :boolean, aliases: "-c",
                    default: false, group: :runtime,
                    desc: "create mod Card gem"
 
       def create_directories
-        Dir.chdir(self.class.source_root) do
-          Dir.glob("**/*").each do |path|
-            empty_directory File.join(mod_path, path)
+        empty_directory mod_path
+        inside mod_path do
+          Dir.chdir(self.class.source_root) do
+            Dir.glob("**/*").each do |path|
+              empty_directory path
+            end
           end
         end
       end
