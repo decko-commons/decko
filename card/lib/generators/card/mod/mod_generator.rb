52084b6b3f1b75f47f8c21b5d7366da25778566f
xithan <philipp.kuehl@gmail.com>
2018-06-20 16:14:15 -0400

    fix mod generator

--- card/lib/generators/card/mod/mod_generator.rb
+++ card/lib/generators/card/mod/mod_generator.rb
@@ -5,20 +5,31 @@ require "generators/card"
 class Card
   module Generators
     class ModGenerator < NamedBase
-      source_root File.expand_path("../templates", __FILE__)
-
       class_option "core",
                    type: :boolean, aliases: "-c",
                    default: false, group: :runtime,
                    desc: "create mod Card gem"
 
-      def create_directories
-        empty_directory mod_path
-        inside mod_path do
-          Dir.chdir(self.class.source_root) do
-            Dir.glob("**/*").each do |path|
-              empty_directory path
+      def create_mod_tree
+        create_empty_tree mod_path => { lib:    [:javascript, :stylesheets],
+                                        public: [:assets],
+                                        set:    [] }
+      end
+
+      private
+
+      def create_empty_tree structure
+        return unless structure.present?
+        if structure.is_a?(Hash)
+          structure.each_pair do |k, v|
+            empty_directory k.to_s
+            inside k.to_s do
+              create_empty_tree v
             end
+          end
+        else
+          Array.wrap(structure).each do |v|
+            empty_directory v.to_s
           end
         end
       end
