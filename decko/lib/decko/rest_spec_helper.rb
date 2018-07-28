b249f93995a46a53996d846243254b184c47fdd3
Ethan McCutchen <ethan@grasscommons.org>
2018-07-27 22:50:13 -0400

    fix sign up link error

--- decko/lib/decko/rest_spec_helper.rb
+++ decko/lib/decko/rest_spec_helper.rb
@@ -2,11 +2,18 @@
 
 module Decko
   # For use in REST API specs
+  module RestSpecMethods
+    def with_token_for usermark
+      yield Card[usermark].account.reset_token
+    end
+  end
+
   module RestSpecHelper
     def self.describe_api &block
       RSpec.describe CardController, type: :controller do
         routes { Decko::Engine.routes }
         include Capybara::DSL
+        include RestSpecMethods
         instance_eval(&block)
       end
     end
