18931cb9602f64bf4b511df37a4c42c027f19302
dcastroeyss <daniel.castro@eyss-dev.com>
2018-08-02 17:10:41 +0200

    tr in permissions and rename

--- card/mod/core/set/all/rename.rb
+++ card/mod/core/set/all/rename.rb
@@ -16,9 +16,9 @@ end
 
 event :validate_renaming, :validate, on: :update, changed: :name do
   if db_content_is_changing?
-    errors.add :content, "cannot change content while changing name"
+    errors.add :content, tr(:cannot_change_content)
   end
-  errors.add :type, "cannot change type while changing name" if type_id_is_changing?
+  errors.add :type, tr(:cannot_change_type) if type_id_is_changing?
 end
 
 event :cascade_name_changes, :finalize, on: :update, changed: :name,
