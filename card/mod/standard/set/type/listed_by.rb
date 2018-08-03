8a8e9483789bf30cea761ab0512b07644e734fa1
dcastroeyss <daniel.castro@eyss-dev.com>
2018-08-03 13:08:41 +0200

    fixed error in listed_rb

--- card/mod/standard/set/type/listed_by.rb
+++ card/mod/standard/set/type/listed_by.rb
@@ -1,8 +1,8 @@
 include_set Pointer
 
-event :validate_list_name, :validate, on: :save, changed: :name do # diferent
+event :validate_listed_by_name, :validate, on: :save, changed: :name do
   if !junction? || !right || right.type_id != CardtypeID
-    errors.add :name, tr(:cardtype_right)
+    errors.add :name, "must have a cardtype name as right part"
   end
 end
 
@@ -10,7 +10,11 @@ event :validate_listed_by_content, :validate,
       on: :save, changed: :content do
   item_cards(content: content).each do |item_card|
     next unless item_card.type_id != right.id
-    errors.add(:content, tr(:only_type, cardname: item_card.name, cardtype: name.right))
+    errors.add(
+      :content,
+      "#{item_card.name} has wrong cardtype; " \
+      "only cards of type #{name.right} are allowed"
+    )
   end
 end
 
