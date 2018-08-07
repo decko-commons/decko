37d1c7e41b37deb2d708501a88051f8328b8eced
dcastroeyss <daniel.castro@eyss-dev.com>
2018-08-07 10:15:02 +0200

    correcting syntax error

--- card/mod/account/set/right/account.rb
+++ card/mod/account/set/right/account.rb
@@ -12,9 +12,7 @@ card_accessor :token
 
 # legal to add +*account card
 event :validate_accountability, :prepare_to_validate, on: :create do
-  unless left && left.accountable?
-    errors.add :content, tr(:error_not_allowed)
-  end
+  errors.add :content, tr(:error_not_allowed) unless left&.accountable?
 end
 
 event :require_email, :prepare_to_validate,
@@ -195,102 +193,6 @@ format :html do
   end
 end
 
-<<<<<<< HEAD
-event :validate_accountability, :prepare_to_validate, on: :create do
-  errors.add :content, tr(:error_not_allowed) unless left && left.accountable?
-end
-
-event :require_email, :prepare_to_validate,
-      after: :validate_accountability, on: :create do
-  errors.add :email, tr(:required) unless subfield(:email)
-end
-
-event :set_default_salt, :prepare_to_validate, on: :create do
-  salt = Digest::SHA1.hexdigest "--#{Time.zone.now}--"
-  Env[:salt] = salt # HACK!!! need viable mechanism to get this to password
-  add_subfield :salt, content: salt
-end
-
-event :set_default_status, :prepare_to_validate, on: :create do
-  default_status = Auth.needs_setup? ? "active" : "pending"
-  add_subfield :status, content: default_status
-end
-
-def can_approve?
-  Card.new(type_id: Card.default_accounted_type_id).ok? :create
-end
-
-event :generate_confirmation_token,
-      :prepare_to_store, on: :create, when: :can_approve? do
-  add_subfield :token, content: generate_token
-end
-
-event :reset_password,
-      :prepare_to_validate, on: :update, when: :reset_password? do
-  valid = validate_token! @env_token
-  success << (valid ? reset_password_success : reset_password_try_again)
-  abort :success
-end
-
-def reset_password_success
-  token_card.used!
-  Auth.signin left_id
-  { id: left.name,
-    view: :related,
-    slot: { items: { nest_name: :account.cardname.prepend_joint,
-                     view: :edit } } }
-end
-
-def reset_password_try_again
-  send_reset_password_token
-  { id: "_self",
-    view: "message",
-    message: tr(:sorry_email_reset, error_msg: errors.first.last) }
-end
-
-def edit_password_success_args; end
-
-def reset_password?
-  @env_token = Env.params[:token]
-  @env_token && Env.params[:event] == "reset_password"
-end
-
-event :reset_token do
-  Auth.as_bot do
-    token_card.update_attributes! content: generate_token
-  end
-end
-
-event :send_welcome_email do
-  welcome = Card[:welcome_email]
-  welcome.deliver self, to: email if welcome&.type_code == :email_template
-end
-
-event :send_account_verification_email, :integrate,
-      on: :create, when: proc { |c| c.token.present? } do
-  Card[:verification_email].deliver self, to: email
-end
-
-event :send_reset_password_token do
-  Auth.as_bot do
-    token_card.update_attributes! content: generate_token
-  end
-  Card[:password_reset_email].deliver self, to: email
-end
-
-def ok_to_read
-  own_account? ? true : super
-end
-
-def changes_visible? act
-  act.actions_affecting(act.card).each do |action|
-    return true if action.card.ok? :read
-  end
-  false
-end
-
-=======
->>>>>>> f12ca4fa4a613399609e8fa9b1058bd5961d380b
 format :email do
   def mail context, fields
     super context, fields.reverse_merge(to: card.email)
