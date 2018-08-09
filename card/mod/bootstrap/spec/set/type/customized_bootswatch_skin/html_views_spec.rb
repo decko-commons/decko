13c54c646ecef36a3b81fac077fdeec04ef06d5b
dcastroeyss <daniel.castro@eyss-dev.com>
2018-08-09 10:50:49 +0200

    correcting syntax error

--- card/mod/bootstrap/spec/set/type/customized_bootswatch_skin/html_views_spec.rb
+++ card/mod/bootstrap/spec/set/type/customized_bootswatch_skin/html_views_spec.rb
@@ -1,6 +1,6 @@
 RSpec.describe Card::Set::Type::CustomizedBootswatchSkin::HtmlViews do
   specify "view core" do
-    expect_view(:core).to have_tag("div.card-slot.missing-view") do 
+    expect_view(:core).to have_tag("div.card-slot.missing-view") do
       with_tag "a.slotter.missing-bar"
     end
   end
--- card/mod/settings/spec/set/abstract/permission_spec.rb
+++
@@ -1,5 +0,0 @@
-# -*- encoding : utf-8 -*-
-
-describe Card::Set::Abstract::Permission do
-  # (just aliases so far)
-end
