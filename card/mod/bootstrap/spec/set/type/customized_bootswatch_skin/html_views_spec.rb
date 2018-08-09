f3d1dd9e31ed72fe67a2e2eae07a632f9391aaa2
dcastroeyss <daniel.castro@eyss-dev.com>
2018-08-09 11:43:56 +0200

    cambio ficticio

--- card/mod/bootstrap/spec/set/type/customized_bootswatch_skin/html_views_spec.rb
+++ card/mod/bootstrap/spec/set/type/customized_bootswatch_skin/html_views_spec.rb
@@ -1,5 +1,5 @@
 RSpec.describe Card::Set::Type::CustomizedBootswatchSkin::HtmlViews do
   specify "view core" do
-    expect_view(:core).to have_tag("div.card-slot.missing-view")
+    expect_view(:core).to have_tag("h5")
   end
 end
