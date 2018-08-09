350d2db201e57c0d3e4a5183ce73be4ed4dfefc5
dcastroeyss <daniel.castro@eyss-dev.com>
2018-08-09 11:26:01 +0200

    correcting rspec erros

--- card/mod/bootstrap/spec/set/type/customized_bootswatch_skin/html_views_spec.rb
+++ card/mod/bootstrap/spec/set/type/customized_bootswatch_skin/html_views_spec.rb
@@ -1,7 +1,5 @@
 RSpec.describe Card::Set::Type::CustomizedBootswatchSkin::HtmlViews do
   specify "view core" do
-    expect_view(:core).to have_tag("div.card-slot.missing-view") do
-      with_tag "a.slotter.missing-bar"
-    end
+    expect_view(:core).to have_tag("div.card-slot.missing-view")
   end
 end
