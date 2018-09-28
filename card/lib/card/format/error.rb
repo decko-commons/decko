d2778dc72d260f14111c3c5a4fca6be02848a261
Ethan McCutchen <ethan@grasscommons.org>
2018-09-27 21:26:10 -0400

    raise errors in test env

--- card/lib/card/format/error.rb
+++ card/lib/card/format/error.rb
@@ -3,7 +3,7 @@ class Card
     module Error
       def rescue_view e, view
         # make config option; don't refer directly to env
-        raise e if Rails.env =~ /^cucumber$/
+        raise e if Rails.env =~ /^cucumber|test$/
         # TODO: unify with Card::Error#report
         Rails.logger.info "#{e.message}\n#{e.backtrace}"
         method = focal? ? :focal_error : :rendering_error
