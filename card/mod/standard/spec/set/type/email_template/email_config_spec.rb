815e00b0f8795eb17c289f42447cb75df4c199ad
Ethan McCutchen <ethan@grasscommons.org>
2018-10-29 16:55:38 -0400

    fix specs broken by new title handling

--- card/mod/standard/spec/set/type/email_template/email_config_spec.rb
+++ card/mod/standard/spec/set/type/email_template/email_config_spec.rb
@@ -131,8 +131,8 @@ describe Card::Set::Type::EmailTemplate::EmailConfig do
       Card::Env[:protocol] = "http://"
       Card::Env[:host] = "www.fake.com"
       is_expected.to include 'Card link(<a class="known-card" '\
-                               'href="http://www.fake.com/A">' \
-                               '<span class="card-title">A</span></a>)'
+                             'href="http://www.fake.com/A">' \
+                             '<span class="card-title" title="A">A</span></a>)'
     end
   end
 
