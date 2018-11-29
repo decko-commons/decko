5743e5dbf233192d5ac4e0e1d884a96a2ef5a1b8
Ethan McCutchen <ethan@grasscommons.org>
2018-11-29 10:51:03 -0500

    fixes for ubos sites: headers, skin images, and */*

--- card/mod/bootstrap/db/migrate_core_cards/data/20181108181219_migrate_classic_skins_to_bootstrap.rb
+++ card/mod/bootstrap/db/migrate_core_cards/data/20181108181219_migrate_classic_skins_to_bootstrap.rb
@@ -10,10 +10,19 @@ class MigrateClassicSkinsToBootstrap < ActiveRecord::Migration[5.2]
 
   DEFAULT_SKIN = "yeti skin".freeze
 
+  UNSUPPORTED_LAYOUT = "classic_layout"
+
+  DEFAULT_LAYOUT = "Default Layout"
+
   def change
     style_rule = Card[:all, :style]
     if style_rule.item_names.first.key.in? UNSUPPORTED_SKINS
       style_rule.update_attributes! content: DEFAULT_SKIN
+    end
+
+    layout_rule = Card[:all, :layout]
+    if layout_rule.item_names.first.key == UNSUPPORTED_LAYOUT
+      layout_rule.update_attributes! content: DEFAULT_LAYOUT
     end
   end
 end
