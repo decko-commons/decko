cfa331675652d2ee14a2786c54b0f8a004a4e92e
Philipp Kühl <philipp.kuehl@gmail.com>
2019-01-25 17:28:34 +0100

    fix emailhtmlformat spec

--- card/mod/standard/spec/set/all/email_html_format_spec.rb
+++ card/mod/standard/spec/set/all/email_html_format_spec.rb
@@ -1,12 +1,12 @@
 # -*- encoding : utf-8 -*-
 
-describe Card::Format::EmailHtmlFormat do
+RSpec.describe Card::Format::EmailHtmlFormat do
   it "renders full urls" do
     Card::Env[:protocol] = "http://"
     Card::Env[:host] = "www.fake.com"
     expect(render_content("[[B]]", format: "email_html"))
       .to eq('<a class="known-card" href="http://www.fake.com/B">'\
-                     '<span class="card-title">B</span></a>')
+                     '<span class="card-title" title="B">B</span></a>')
   end
 
   describe "raw view" do
