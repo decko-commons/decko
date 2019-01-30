460f3aacbda57f3ce1a9c7ffa66f48854f5d01b7
Philipp Kühl <philipp.kuehl@gmail.com>
2019-01-30 10:45:10 +0100

    remove rule navbar

--- card/mod/rules/set/type/set/html_views.rb
+++ card/mod/rules/set/type/set/html_views.rb
@@ -2,87 +2,41 @@ format :html do
   COMMON_RULE_SETTINGS =
     %i[create read update delete structure default].freeze
 
-  view :core, cache: :never do
-    voo.show :set_label, :rule_navbar
-    voo.hide :set_navbar
-    rule_view = params[:rule_view] || :common_rules
+  before :open do
+    voo.hide :template_closer
+  end
 
-    table_rules_filter + _render(rule_view)
+  view :core, cache: :never do
+    voo.show :set_label
+    voo.hide :set_navbar, :rule_navbar
+
+    table_rules_filter + _render_rules_table
+  end
+
+  view :rules_table, cache: :never do
+    rules_table setting_list(setting_group)
   end
 
   def table_rules_filter
-    form_tag path(mark: "", view: :rules_list,
+    form_tag path(view: :rules_table,
                   slot: { hide: %i[set_label rule_navbar set_navbar content] }),
              remote: true, method: "get", role: "filter",
-             "data-slot-selector": ".card-slot.rules_list-view",
+             "data-slot-selector": ".card-slot.rules-table",
              class: classy("nodblclick slotter form-inline slim-select2 m-2") do
       output [
                label_tag(:view, icon_tag("filter_list"), class: "mr-2"),
                setting_select,
-               content_tag(:span, "rules that apply to set #{card.name}", class: "mx-2 small"),
+               content_tag(:span, "rules that apply to #{_render_set_label}".html_safe, class: "mx-2 small"),
              ]
     end
   end
 
-  def with_label_and_navbars selected_view
-    @selected_rule_navbar_view = selected_view
-    wrap do
-      [
-        # _render_set_label,
-        # _render_set_navbar,
-        _render_rule_navbar,
-        yield
-      ]
-    end
-  end
-
-  view :all_rules do
-    with_label_and_navbars :all_rules do
-      rules_table card.visible_setting_codenames.sort
-    end
-  end
-
-  view :grouped_rules do
-    with_label_and_navbars :grouped_rules do
-      wrap_with :div, class: "panel-group", id: "accordion",
-                      role: "tablist", "aria-multiselectable": "true" do
-        Card::Setting.groups.keys.map do |group_key|
-          _render group_key
-        end
-      end
-    end
-  end
-
-  view :recent_rules do
-    with_label_and_navbars :recent_rules do
-      recent_settings = Card[:recent_settings].item_cards.map(&:codename)
-      settings = recent_settings.map(&:to_sym) & card.visible_setting_codenames
-      rules_table settings
-    end
-  end
-
-  view :common_rules do
-    with_label_and_navbars :common_rules do
-      # "&" = set intersection
-      settings = card.visible_setting_codenames & COMMON_RULE_SETTINGS
-      rules_table settings
-    end
-  end
-
-  view :field_related_rules do
-    with_label_and_navbars :field_related_rules do
-      field_settings = %i[default help structure]
-      if card.type_id == PointerID
-        # FIXME: should be done with override in pointer set module
-        field_settings += %i[input options options_label]
-      end
-      settings = card.visible_setting_codenames & field_settings
-      rules_table settings
-    end
+  def setting_group
+    params[:group]&.to_sym || :common
   end
 
   view :set_label do
-    wrap_with :h3, card.label, class: "set-label"
+    wrap_with :strong, card.label, class: "set-label"
   end
 
   Card::Setting.groups.each_key do |group_key|
@@ -94,8 +48,11 @@ format :html do
   end
 
   def rules_table settings
-    haml :rules_table, settings: settings,
-                       item_view: voo.show?(:content) ? :closed_rule : :rule_link
+    class_up "card-slot", "rules-table"
+    wrap do
+      haml :rules_table, settings: settings,
+                         item_view: voo.show?(:content) ? :closed_rule : :rule_link
+    end
   end
 
   def rule_table_row setting
@@ -114,42 +71,5 @@ format :html do
 
   view :closed_content do
     ""
-  end
-
-  def li_pill content, active
-    "<li role='presentation' class='nav-item #{'active' if active}'>#{content}</li>"
-  end
-
-  view :rule_navbar do
-    navbar "rule-navbar-#{card.name.safe_key}-#{voo.home_view}",
-           brand: "Rules", toggle_align: :right,
-           class: "slotter toolbar navbar-expand-md bg-dark", navbar_type: "dark",
-           collapsed_content: close_link("float-right d-sm-none") do
-      rule_navbar_content
-    end
-  end
-
-  def rule_navbar_pills
-    pills = [["common",   :common_rules],
-             ["by group", :grouped_rules],
-             ["by name",  :all_rules]]
-    pills.unshift ["field", :field_related_rules] if card.junction?
-    pills.push ["recent", :recent_rules] if recently_edited_settings?
-    pills
-  end
-
-  def rule_navbar_content
-    wrap_with :ul, class: "nav navbar-nav nav-pills" do
-      rule_navbar_pills.map do |label, symbol|
-        view_link_pill label, symbol
-      end
-    end
-  end
-
-  def view_link_pill name, view
-    selected_view = @selected_rule_navbar_view || @slot_view || voo.home_view
-    link = link_to_view view, name, class: "nav-link slotter", role: "pill",
-                                    path: { slot: { show: :rule_navbar } }
-    li_pill link, selected_view == view
   end
 end
