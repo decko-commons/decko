3b340df3749439bfdae948d29f9b7756eb7563e4
Philipp Kühl <philipp.kuehl@gmail.com>
2019-02-06 10:53:59 +0100

    fix rule specs

--- card/mod/rules/spec/type/set/html_views_spec.rb
+++ card/mod/rules/spec/type/set/html_views_spec.rb
@@ -6,4 +6,13 @@ RSpec.describe Card::Set::Type::Set::HtmlViews do
   end
 
   check_html_views_for_errors
+
+  it "renders setting view for a right set" do
+    r = Card["*read+*right"].format.render_open
+    # warn "r = #{r}"
+    assert_view_select r, 'table[class="set-rules table"]' do
+      assert_select 'a[href~="/*read+*right+*read?view=open_rule"]',
+                    text: "read"
+    end
+  end
 end
