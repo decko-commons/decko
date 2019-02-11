fc69ef16afadfe245b0dea86e5c514b4ed1dd2f1
Philipp Kühl <philipp.kuehl@gmail.com>
2019-02-11 19:45:08 +0100

    fix cypress_folder path

--- decko/lib/decko/generators/decko/templates/config/initializers/cypress_dev.rb
+++ decko/lib/decko/generators/decko/templates/config/initializers/cypress_dev.rb
@@ -1,9 +1,9 @@
 if defined?(CypressDev)
   CypressDev.configure do |c|
-    c.cypress_folder = File.expand_path("#{__dir__}/../../spec/cypress")
+    c.cypress_folder = File.join Decko.gem_root, "spec", "cypress"
     # WARNING!! CypressDev can execute arbitrary ruby code
     # please use with extra caution if enabling on hosted servers or starting your local server on 0.0.0.0
-    c.use_middleware = Rails.env.test?
+    c.use_middleware = Rails.env.test? || ENV["CYPRESS_DEV"]
     c.logger = Rails.logger
   end
 end
