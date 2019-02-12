cb96cfbbd1d2af288dd521b4e1fe2c2818c1e6a1
Philipp Kühl <philipp.kuehl@gmail.com>
2019-02-12 15:41:08 +0100

    fix layout migration again

--- card/db/migrate_core_cards/20180905061536_migrate_layouts.rb
+++ card/db/migrate_core_cards/20180905061536_migrate_layouts.rb
@@ -3,45 +3,41 @@
 class MigrateLayouts < Card::Migration::Core
   def up
     Card.search referred_to_by: { right: :layout } do |card|
-      update_layout_card card
       create_head_rules card
+      update_layout_card card
     end
   end
 
   # strip layout content down to body tag
   def update_layout_card card
-    body = html_tag card, "body"
+    body = body_tag card
     missing "body", card  unless body.present?
 
     puts "updating layout '#{card.name}'"
     card.update! content: body.to_s
   end
 
+  def body_tag card
+    card.content[/<body[^>]+>.*<\/body>/mi]
+  end
+
   def create_head_rules layout_card
     head = find_head_content layout_card
     return missing "head", layout_card unless head
+
     each_layout_set layout_card do |set_name|
       puts "creating head rule for '#{set_name}'"
-      ensure_card [set_name, :head], content: head,
-                  type_id: Card::HtmlID
+      ensure_card [set_name, :head], content: head, type_id: Card::HtmlID
     end
   end
 
   def find_head_content card
-    head = Nokogiri::XML(card.content).css("head")
-    return head.first.text if head.present?
-
-    # try to find head in nests
-    # we go only one level deep
-    Card::Content.new(card.content, card, chunk_list: :nest_only).each_chunk do |chunk|
-      if chunk.referee_card&.content&.include? "<head>"
-        return Nokogiri::XML(chunk.referee_card.content).css("head").first.text
-      end
-    end
+    head_tag(card) || find_nested_head(card)
   end
 
-  def html_tag card, tag_name
-    Nokogiri::HTML5(card.content).css(tag_name).first
+  def head_tag card
+    head = Nokogiri::HTML5(card.content).css "head"
+    head&.first&.text
   end
 
   def missing obj, card
@@ -51,6 +47,15 @@ class MigrateLayouts < Card::Migration::Core
   def each_layout_set layout_card
     Card.search(link_to: layout_card.name) do |rule|
       rule.name.left
+    end
+  end
+
+  private
+
+  def find_nested_head card
+    # we go only one level deep
+    Card::Content.new(card.content, card, chunk_list: :nest_only).each_chunk do |chunk|
+      return head_tag chunk.referee_card if chunk.referee_card&.content&.match(/<head>/i)
     end
   end
 end
