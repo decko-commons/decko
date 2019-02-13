adebe002555590bf81edf78de6f6355fca43fe0d
Philipp Kühl <philipp.kuehl@gmail.com>
2019-02-13 17:21:59 +0100

    no double "#main"

--- card/lib/card/view/layout.rb
+++ card/lib/card/view/layout.rb
@@ -2,12 +2,13 @@ class Card
   class View
     # method to render views with layouts
     module Layout
-      def with_layout &render_block
-        return yield unless layout.present?
-
-        format.rendered_main_nest = format.wrap_main(&render_block)
-        ::Card::Layout.render layout, self
-      end
+      # def with_layout &render_block
+      #   binding.pry
+      #   return yield unless layout.present?
+      #
+      #   format.rendered_main_nest = format.wrap_main(&render_block)
+      #   ::Card::Layout.render layout, self
+      # end
     end
   end
 end
