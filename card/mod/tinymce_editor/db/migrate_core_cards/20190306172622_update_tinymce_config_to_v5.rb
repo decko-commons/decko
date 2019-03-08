cd2c1ed1814f495a0fa9dd4b89d1bae889aa6012
Philipp Kühl <philipp.kuehl@gmail.com>
2019-03-08 18:01:47 +0100

    more tinymce upgrade fixes

--- card/mod/tinymce_editor/db/migrate_core_cards/20190306172622_update_tinymce_config_to_v5.rb
+++
@@ -1,8 +0,0 @@
-# -*- encoding : utf-8 -*-
-
-class UpdateTinymceConfigToV5 < Card::Migration::Core
-  def up
-    content = Card[:tiny_mce].content.sub('"modern"', '"silver"')
-    ensure_card :tiny_mce, content: content
-  end
-end
