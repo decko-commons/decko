3caff4d38d1c1f2dd44c99cc4583a75e15a1bd14
xithan <philipp.kuehl@gmail.com>
2019-03-22 22:59:27 +0100

    fix logo migration

--- card/db/migrate_core_cards/20190321085735_wagn_to_decko_logo.rb
+++ card/db/migrate_core_cards/20190321085735_wagn_to_decko_logo.rb
@@ -3,9 +3,10 @@
 class WagnToDeckoLogo < Card::Migration::Core
   def up
     logo = Card[:logo]
+    binding.pry
     return unless logo&.pristine?
 
-    logo.update! content: ":logo/standard.svg",
-                 empty_ok: true
+    logo.update_column :db_content, ":logo/standard.svg"
+    Card::Cache.reset_all
   end
 end
