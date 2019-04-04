de7c216fda2dc495c71d3207db827ffeb87283b8
Philipp Kühl <philipp.kuehl@gmail.com>
2019-04-04 10:24:32 +0200

    turn off bulk insert

--- card/mod/history/set/all/history/events.rb
+++ card/mod/history/set/all/history/events.rb
@@ -60,9 +60,10 @@ end
 
 def store_each_history_field action_id, fields=nil
   fields ||= Card::Change::TRACKED_FIELDS
-  if Card::Change.supports_import?
+  if false # Card::Change.supports_import?
+    # attach.feature fails with this
     values = fields.map.with_index { |field, index| [index, yield(field), action_id] }
-    Card::Change.import [:field, :value, :card_action_id], values, validate: false
+    Card::Change.import [:field, :value, :card_action_id], values #, validate: false
   else
     fields.each do |field|
       Card::Change.create field: field,
