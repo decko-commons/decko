4a8dcf130e630e93266fd015ff4dd543a83677a6
Ethan McCutchen <ethan@grasscommons.org>
2019-05-16 23:24:26 -0400

    geez, migration typo

--- card/db/migrate_core_cards/20190424101004_add_read_anyone_rules.rb
+++ card/db/migrate_core_cards/20190424101004_add_read_anyone_rules.rb
@@ -3,7 +3,7 @@
 class AddReadAnyoneRules < Card::Migration::Core
   def up
     ensure_card "*main menu", codename: :main_menu
-    cards = %i[title main menu navbox credit credit_image].map { |c| [c, :self] }
+    cards = %i[title main_menu navbox credit credit_image].map { |c| [c, :self] }
     cards << %i[head right]
     cards.each do |codename, set|
       ensure_card [codename, set, :read], type_id: Card::PointerID, content: "Anyone"
