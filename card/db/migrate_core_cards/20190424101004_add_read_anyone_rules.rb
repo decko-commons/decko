62ba797a0039e9777ad8c82ffb9c0333deec8c1d
Ethan McCutchen <ethan@grasscommons.org>
2019-05-16 23:29:38 -0400

    ...

--- card/db/migrate_core_cards/20190424101004_add_read_anyone_rules.rb
+++ card/db/migrate_core_cards/20190424101004_add_read_anyone_rules.rb
@@ -3,6 +3,7 @@
 class AddReadAnyoneRules < Card::Migration::Core
   def up
     ensure_card "*main menu", codename: :main_menu
+    ensure_card "*credit", codename: :credit
     cards = %i[title main_menu navbox credit credit_image].map { |c| [c, :self] }
     cards << %i[head right]
     cards.each do |codename, set|
