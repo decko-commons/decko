094adcae556d0cc8ed47a28e275ee616bbf1458f
Ethan McCutchen <ethan@grasscommons.org>
2019-06-12 22:27:02 -0400

    secure slot options and offset/limit max

--- card/lib/card/env.rb
+++ card/lib/card/env.rb
@@ -47,7 +47,7 @@ class Card
           opts = params[:slot]&.clone || {}
           opts = opts.to_unsafe_h if opts.is_a? ActionController::Parameters
           opts.merge! shortcut_slot_opts
-          opts.deep_symbolize_keys
+          opts.deep_symbolize_keys.slice(*Card::View::Options.shark_keys)
         end
       end
 
@@ -119,7 +119,7 @@ class Card
       end
 
       def slot_items_shortcut?
-        params[:item].present? && !params.dig(:slot, :items, :view).present?
+        params[:item].present? && !slot_opts.dig(:items, :view).present?
       end
     end
   end
