32825d60785e444559e70cd28045838c21afe416
Ethan McCutchen <ethan@grasscommons.org>
2019-08-30 19:23:04 -0400

    reseeded

--- card/mod/settings/set/self/accountable.rb
+++
@@ -1,5 +0,0 @@
-require_dependency "card/setting"
-extend Card::Setting
-setting_opts group: :other, position: 2,
-             help_text: "[[http://decko.org/custom_help_text|Help text]] people will "\
-                        "see when editing."
