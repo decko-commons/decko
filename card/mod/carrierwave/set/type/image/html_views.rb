9cc9ced3c0ab0df6e1c0a316eb98fb4ea97cf3b0
Ethan McCutchen <ethan@grasscommons.org>
2019-09-02 12:54:41 -0400

    move image-box styling from wikirate to decko (card)

--- card/mod/carrierwave/set/type/image/html_views.rb
+++ card/mod/carrierwave/set/type/image/html_views.rb
@@ -1,7 +1,3 @@
-IMAGE_BOX_SIZE_MAP = {
-  icon: :icon, small: :small, medium: :small, large: :medium, xlarge: :medium
-}.freeze
-
 format :html do
   include File::HtmlFormat
 
@@ -69,36 +65,5 @@ format :html do
     card.with_selected_action_id action.id do
       Card::Content::Diff.render_added_chunk _render_core
     end
-  end
-
-  view :boxed, unknown: true do
-    image_box { |size| render_core size: size }
-  end
-
-  view :boxed_link, unknown: true do
-    image_box { |size| link_to_card image_box_link_target, render_core(size: size) }
-  end
-
-  def image_box
-    voo.size ||= :medium
-    wrap_with :div, title: image_box_title, class: "image-box #{voo.size}" do
-      yield image_box_size
-    end
-  end
-
-  def image_box_size
-    IMAGE_BOX_SIZE_MAP[voo.size.to_sym] || :medium
-  end
-
-  def image_box_card_name
-    card.name.junction? ? card.name.left : card.name
-  end
-
-  def image_box_link_target
-    image_box_card_name
-  end
-
-  def image_box_title
-    image_box_card_name
   end
 end
---
+++ card/mod/media/set/self/style_media.rb
@@ -0,0 +1,6 @@
+include_set Abstract::CodeFile
+Self::StyleMods.add_item :style_media
+
+def source_files
+  scss_files [:image_box]
+end
