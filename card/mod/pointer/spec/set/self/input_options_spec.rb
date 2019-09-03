ba0744da6697b9aa19a9ae31bbceab4cb5a8c6e2
Ethan McCutchen <ethan@grasscommons.org>
2019-09-03 10:22:38 -0400

    input_options_spec fix

--- card/mod/pointer/spec/set/self/input_options_spec.rb
+++ card/mod/pointer/spec/set/self/input_options_spec.rb
@@ -1,6 +1,6 @@
 describe Card::Set::Self::InputOptions do
   it "loads the self set" do
-    expect(Card[:input, :right, :options].item_names).to contain_exactly(
+    expect(Card[:input_type, :right, :content_options].item_names).to contain_exactly(
       "radio", "checkbox", "select", "multiselect", "list", "ace editor", "filtered list",
       "prosemirror editor", "tinymce editor", "text area", "text field", "calendar"
     )
