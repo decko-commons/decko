c44fce11aacc2b417e622b31c5e42023e5341f3f
Philipp Kühl <philipp.kuehl@gmail.com>
2019-09-09 16:23:51 +0200

    Add migration

--- card/db/migrate_core_cards/20190909104250_add_cardtype_input_types.rb
+++ card/db/migrate_core_cards/20190909104250_add_cardtype_input_types.rb
@@ -2,6 +2,17 @@
 
 class AddCardtypeInputTypes < Card::Migration::Core
   def up
-    ensure_card [:pointer, :input_type]
+    ensure_input_types pointer: %w[select radio autocomplete],
+                       list: ["multiselect", "checkbox", "filtered list",
+                              "autocompleted list"],
+                       plain_text: ["text area", "text field", "ace editor"]
+  end
+
+  def ensure_input_types hash
+    # hash.each do |cardtype, input_types|
+    #   ensure_card [cardtype, :input_type, :type_plus_right],
+    #               type_id: Card::ListID,
+    #               content: input_types
+    #   end
   end
 end
