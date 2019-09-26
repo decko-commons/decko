f9926a32583958fecc5f363bdd395326688074a7
xithan <philipp.kuehl@gmail.com>
2019-09-26 18:48:15 +0200

    Make smart label the default content option view

--- card/db/migrate_core_cards/20190909104250_add_cardtype_input_types.rb
+++ card/db/migrate_core_cards/20190909104250_add_cardtype_input_types.rb
@@ -4,5 +4,7 @@ class AddCardtypeInputTypes < Card::Migration::Core
   def up
     ensure_card [:input_type, :right, :default],
                 type_id: Card::PointerID
+    ensure_card [:content_option_view, :right, :default],
+                type_id: "smart label"
   end
 end
