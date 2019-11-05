c13eb0eb92861adc3c1f57806c9ffc367f654e90
Ethan McCutchen <ethan@grasscommons.org>
2019-11-05 11:23:58 -0500

    again reverting view cache handling to json

--- card/lib/card/view/cache/stub.rb
+++ card/lib/card/view/cache/stub.rb
@@ -15,8 +15,8 @@ class Card
 
         # @return [String]
         def stub
-          # "(StUb#{stub_hash.to_json}sTuB)".html_safe
-          "(StUb#{bin_to_hex stub_hash.to_msgpack}sTuB)".html_safe
+          "(StUb#{stub_hash.to_json}sTuB)".html_safe
+          # "(StUb#{bin_to_hex stub_hash.to_msgpack}sTuB)".html_safe
         end
 
         def bin_to_hex string
