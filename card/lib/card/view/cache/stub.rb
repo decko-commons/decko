db46012d4378d8450f216a7b6ec10d798c307d70
Ethan McCutchen <ethan@grasscommons.org>
2019-11-05 11:18:22 -0500

    again reverting to json

--- card/lib/card/view/cache/stub.rb
+++ card/lib/card/view/cache/stub.rb
@@ -15,6 +15,7 @@ class Card
 
         # @return [String]
         def stub
+          # "(StUb#{stub_hash.to_json}sTuB)".html_safe
           "(StUb#{bin_to_hex stub_hash.to_msgpack}sTuB)".html_safe
         end
 
--- card/mod/core/chunk/view_stub.rb
+++ card/mod/core/chunk/view_stub.rb
@@ -21,8 +21,8 @@ class Card
         end
 
         def initial_stub_hash string
-          # JSON.parse(string).symbolize_keys
-          MessagePack.unpack(hex_to_bin(string)).symbolize_keys
+          JSON.parse(string).symbolize_keys
+          # MessagePack.unpack(hex_to_bin(string)).symbolize_keys
         end
 
         def hex_to_bin string
