a5326fe66281704a521ea510758e69a5f1a64c88
Ethan McCutchen <ethan@grasscommons.org>
2020-01-10 14:13:58 -0500

    use format_subject in view testing

--- card/spec/support/card_shared_examples.rb
+++ card/spec/support/card_shared_examples.rb
@@ -1,7 +1,7 @@
 shared_examples "view without errors" do |view_name, format=:html|
   # let(:view) { Card.fetch name }
   it "view #{view_name} has no errors" do
-    expect(card_subject.format(format).render(view_name)).to lack_errors
+    expect(format_subject(format).render(view_name)).to lack_errors
   end
 end
 
@@ -10,7 +10,7 @@ shared_examples "view with valid html" do |view_name|
   include RSpecHtmlMatchers::SyntaxHighlighting
   # let(:view) { Card.fetch name }
   it "view #{view_name} has valid html" do
-    rendered = card_subject.format.render(view_name)
+    rendered = format_subject.render(view_name)
     doc = Nokogiri::HTML5.fragment rendered
     expect(doc.errors).to be_empty, [doc.errors, highlight_syntax(rendered)].join("\n")
   end
