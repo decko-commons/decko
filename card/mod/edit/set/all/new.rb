80229d817d819e64de80740bcbe91f64a6a11a1b
Ethan McCutchen <ethan@grasscommons.org>
2020-06-16 22:54:16 -0400

    rollback to get green

--- card/mod/edit/set/all/new.rb
+++ card/mod/edit/set/all/new.rb
@@ -152,8 +152,8 @@ format :html do
 
   view :new_type_formgroup do
     wrap_type_formgroup do
-      type_field class: "type-field live-type-field slotter",
-                 href: path,
+      type_field class: "type-field live-type-field",
+                 href: path(view: :new),
                  "data-remote" => true
     end
   end
