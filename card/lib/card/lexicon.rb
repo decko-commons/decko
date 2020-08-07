18a32e66f09b9dd70738587388dc6cd650ed92dd
Ethan McCutchen <ethan@grasscommons.org>
2020-08-07 13:51:41 -0400

    reseeded with new l/r indexing

--- card/lib/card/lexicon.rb
+++ card/lib/card/lexicon.rb
@@ -1,20 +1,27 @@
 class Card
   module Lexicon
-    # maintain hashes of real cards
+    # Translates keys to ids and vice versa.
+    # Note: the lexicon does NOT distinguish between trashed and untrashed cards.
     class << self
+      # param id [Integer]
+      # @return [String]
       def key id
         id_to_key[id]
       end
 
+      # param name [String]
+      # @return [Integer]
       def id name
         key_to_id[name.to_name.key]
       end
 
+      # @return [Hash] { cardid1 => cardkey1, ...}
       def id_to_key
         @id_to_key ||= Card.cache.fetch("ID-TO-KEY") { generate_id_hash }
         # @id_to_key ||= generate_id_hash
       end
 
+      # @return [Hash] { cardkey1 => cardid1, ...}
       def key_to_id
         @key_to_id ||= Card.cache.fetch("KEY-TO-ID") { id_to_key.invert }
         # @key_to_id ||= id_to_key.invert
@@ -29,13 +36,12 @@ class Card
       def renew
         @id_to_key = nil
         @key_to_id = nil
-        @holder = nil
-        @holder_count = nil
       end
 
       def generate_id_hash
         @id_to_key = {}
         @holder = {}
+        @holder_count = nil
         capture_simple_cards
         capture_compound_cards
         @id_to_key
@@ -53,6 +59,19 @@ class Card
         rewrite
       end
 
+      def delete id, key
+        @id_to_key.delete id
+        @key_to_id.delete key
+        rewrite
+      end
+
+      def update id, key, descendant_ids
+        @id_to_key[id] = key
+        cascade_update descendant_ids
+        @key_to_id = @id_to_key.invert
+        rewrite
+      end
+
       private
 
       def rewrite
@@ -62,6 +81,15 @@ class Card
 
       def raw_rows
         Card.pluck :id, :key, :left_id, :right_id
+      end
+
+      def cascade_update descendant_ids
+        @holder = {}
+        desc = Card.where(id: descendant_ids).pluck(:id, :left_id, :right_id)
+        desc.each do |id, left_id, right_id|
+          @holder[id] = [left_id, right_id]
+        end
+        capture_compound_cards
       end
 
       # record mapping of cards with simple names
