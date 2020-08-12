74cd07a3528791b57471c00b33fc2e0069cd8180
Ethan McCutchen <ethan@grasscommons.org>
2020-08-12 15:59:17 -0400

    more cleanly distinguished cache keys for ids and lexes in Lexicon

--- card/lib/card/lexicon.rb
+++ card/lib/card/lexicon.rb
@@ -6,12 +6,16 @@ class Card
       # param id [Integer]
       # @return [String]
       def key id
+        return unless id.present?
+
         (lex = id_to_lex id) && lex_to_key(lex)
       end
 
       # param name [String]
       # @return [Integer]
       def id name
+        return unless name.present?
+
         (lex = name_to_lex name.to_name) && lex_to_id(lex)
       end
 
@@ -38,7 +42,7 @@ class Card
       end
 
       def id_to_lex id
-        cache.fetch id do
+        cache.fetch id.to_i do
           result = Card.where(id: id).pluck(:key, :left_id, :right_id).first
           return unless result
 
@@ -67,7 +71,7 @@ class Card
       end
 
       def cache_key lex
-        lex.is_a?(Array) ? lex.join("-") : lex
+        "L-" + (lex.is_a?(Array) ? lex.join("-") : lex)
       end
     end
   end
