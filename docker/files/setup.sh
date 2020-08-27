4a078db7ee0de1d476ec049679627133896cbbf0
Gerry Gleason <gerrygleason5700@gmail.com>
2020-08-27 14:02:04 -0500

    Hot patch to disable recaptcha

--- docker/files/setup.sh
+++ docker/files/setup.sh
@@ -1,5 +1,7 @@
 
 su - app sh -c 'cd /home/app/; tar -xzvf /tmp/build/deckoapp.tgz'
+apt-get update -y
+apt-get install -y ruby-dalli
 
 cd /work
 cp /home/app/deckoapp/Gemfile .
@@ -7,7 +9,12 @@ ls -l
 pwd
 bundle install
 
+# turn off captcha gem
+cp /tmp/build/Modfile /usr/local/rvm/gems/ruby-2.7.1/gems/card-1.99.6/mod/Modfile
 mv /tmp/build/env.conf /etc/nginx/main.d/env.conf
+mv /tmp/build/ssl-cert-snakeoil.key /etc/ssl/private/ssl-cert-snakeoil.key
+chmod 400 /etc/ssl/private/ssl-cert-snakeoil.key
+mv /tmp/build/ssl-cert-snakeoil.pem /etc/ssl/certs/ssl-cert-snakeoil.pem
 rm /etc/nginx/sites-enabled/default
 mv /tmp/build/webapp.conf /etc/nginx/sites-enabled/webapp.conf
 rm /etc/service/nginx/down
