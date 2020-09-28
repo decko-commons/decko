1293eecb650f6df88759c57d3b627ae20bcab930
Gerry Gleason <gerrygleason5700@gmail.com>
2020-09-28 13:51:20 -0500

    More card to decko move back

--- decko/lib/decko/commands/application.rb
+++ decko/lib/decko/commands/application.rb
@@ -1,5 +1,5 @@
 require "rails/generators"
-require File.expand_path("../../../../../card/lib/card/generators/card/card_generator", __FILE__)
+require File.expand_path("../../../decko/generators/decko/decko_generator", __FILE__)
 
 if ARGV.first != "new"
   ARGV[0] = "--help"
@@ -7,4 +7,4 @@ else
   ARGV.shift
 end
 
-CardGenerator.start
+DeckoGenerator.start
