f6a095b4a08df2f6ed61dc9c2af5ee09fc5aa4e1
Ethan McCutchen <ethan@grasscommons.org>
2020-10-01 17:13:36 -0400

    added deck_helper

--- decko/lib/decko/generators/deck/deck_generator.rb
+++ decko/lib/decko/generators/deck/deck_generator.rb
@@ -5,7 +5,8 @@ module Decko
     module Deck
       class DeckGenerator < Rails::Generators::AppBase
         require "decko/generators/deck/deck_generator/rails_overrides"
-        require "decko/generators/deck/deck_generator/database_files"
+        require "decko/generators/deck/deck_generator/deck_helper"
+        # require "decko/generators/deck/deck_generator/database_files"
 
         include RailsOverrides
         include DatabaseFiles
@@ -44,6 +45,8 @@ module Decko
         def self.banner
           "decko new #{arguments.map(&:usage).join(' ')} [options]"
         end
+
+        # Generator works its way through each public method below
 
         def rakefile
           template "Rakefile.erb", "Rakefile"
@@ -133,90 +136,11 @@ module Decko
 
         protected
 
-        def shark?
-          !(monkey? || platypus?)
-        end
-
-        def monkey?
-          options[:monkey]
-        end
-
-        def platypus?
-          options[:platypus]
-        end
-
-        def repo_path
-          @repo_path ||= determine_repo_path
-        end
-
-        def determine_repo_path
-          @repo_path_determined ? (return nil) : @repo_path_determined = true
-          path = ENV["DECKO_REPO_PATH"]
-          path.present? ? path.to_s : options["repo-path"]
-          path || prompt_for_repo_path
-        end
-
-        def repo_path_constraint
-          repo_path.present? ? %(, path: "#{repo_path}") : ""
-        end
-
-        def prompt_for_repo_path
-          return if repo_path.present?
-
-          @repo_path = ask "Enter the path to your local decko repository: "
-        end
-
-        def spec_path
-          @spec_path ||= platypus? ? repo_path : "mod/"
-        end
-
-        def spec_helper_path
-          @spec_helper_path ||=
-            platypus? ? "#{repo_path}card/spec/spec_helper" : "./spec/spec_helper"
-        end
-
-        def features_path
-          @features_path ||= platypus? ? "#{repo_path}/decko/features/" : "mod/"
-        end
-
-        # FIXME: these gem roots are not correct unless repo_path is specified
-        def cardio_gem_root
-          @cardio_gem_root ||= File.join repo_path, "card"
-        end
-
-        def decko_gem_root
-          @decko_gem_root ||= File.join repo_path, "decko"
-        end
-
         def database_gemfile_entry
           return [] if options[:skip_active_record]
           gem_name, gem_version = gem_for_database
           msg = "Use #{options[:database]} as the database for Active Record"
           GemfileEntry.version gem_name, gem_version, msg
-        end
-
-        def database_gem_and_version
-          entry = database_gemfile_entry
-          text = %('#{entry.name}')
-          text << %(, '#{entry.version}') if entry.version
-          # single quotes to prevent, eg: `gem "pg", ">= 0.18', '< 2.0"`
-          text
-        end
-
-        def mysql_socket
-          return if RbConfig::CONFIG["host_os"] =~ /mswin|mingw/
-
-          @mysql_socket ||= [
-            "/tmp/mysql.sock",                        # default
-            "/var/run/mysqld/mysqld.sock",            # debian/gentoo
-            "/var/tmp/mysql.sock",                    # freebsd
-            "/var/lib/mysql/mysql.sock",              # fedora
-            "/opt/local/lib/mysql/mysql.sock",        # fedora
-            "/opt/local/var/run/mysqld/mysqld.sock",  # mac + darwinports + mysql
-            "/opt/local/var/run/mysql4/mysqld.sock",  # mac + darwinports + mysql4
-            "/opt/local/var/run/mysql5/mysqld.sock",  # mac + darwinports + mysql5
-            "/opt/lampp/var/mysql/mysql.sock"         # xampp for linux
-          ].find { |f| File.exist?(f) }
         end
       end
     end
