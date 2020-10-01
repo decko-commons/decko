6e89edca1794e709b2e2324c5910af5eb85210cb
Ethan McCutchen <ethan@grasscommons.org>
2020-10-01 17:12:29 -0400

    moving history and account to mods

---
+++ card-mod-history/card-mod-history.gemspec
@@ -0,0 +1,8 @@
+# -*- encoding : utf-8 -*-
+
+require "../decko_gem"
+
+DeckoGem.mod "history" do |s, _d|
+  s.summary = "Revision histories in acts, actions, and changes"
+  s.description = ""
+end
---
+++ decko/lib/decko/generators/deck/deck_generator/deck_helper.rb
@@ -0,0 +1,90 @@
+module Decko
+  module Generators
+    module Deck
+      class DeckGenerator
+        module DeckHelper
+          protected
+          
+          def shark?
+            !(monkey? || platypus?)
+          end
+
+          def monkey?
+            options[:monkey]
+          end
+
+          def platypus?
+            options[:platypus]
+          end
+
+          def repo_path
+            @repo_path ||= determine_repo_path
+          end
+
+          def determine_repo_path
+            @repo_path_determined ? (return nil) : @repo_path_determined = true
+            path = ENV["DECKO_REPO_PATH"]
+            path.present? ? path.to_s : options["repo-path"]
+            path || prompt_for_repo_path
+          end
+
+          def repo_path_constraint
+            repo_path.present? ? %(, path: "#{repo_path}") : ""
+          end
+
+          def prompt_for_repo_path
+            return if repo_path.present?
+
+            @repo_path = ask "Enter the path to your local decko repository: "
+          end
+
+          def spec_path
+            @spec_path ||= platypus? ? repo_path : "mod/"
+          end
+
+          def spec_helper_path
+            @spec_helper_path ||=
+              platypus? ? "#{repo_path}card/spec/spec_helper" : "./spec/spec_helper"
+          end
+
+          def features_path
+            @features_path ||= platypus? ? "#{repo_path}/decko/features/" : "mod/"
+          end
+
+          # FIXME: these gem roots are not correct unless repo_path is specified
+          def cardio_gem_root
+            @cardio_gem_root ||= File.join repo_path, "card"
+          end
+
+          def decko_gem_root
+            @decko_gem_root ||= File.join repo_path, "decko"
+          end
+
+          def database_gem_and_version
+            entry = database_gemfile_entry
+            text = %('#{entry.name}')
+            text << %(, '#{entry.version}') if entry.version
+            # single quotes to prevent, eg: `gem "pg", ">= 0.18', '< 2.0"`
+            text
+          end
+
+          def mysql_socket
+            return if RbConfig::CONFIG["host_os"] =~ /mswin|mingw/
+
+            @mysql_socket ||= [
+              "/tmp/mysql.sock",                        # default
+              "/var/run/mysqld/mysqld.sock",            # debian/gentoo
+              "/var/tmp/mysql.sock",                    # freebsd
+              "/var/lib/mysql/mysql.sock",              # fedora
+              "/opt/local/lib/mysql/mysql.sock",        # fedora
+              "/opt/local/var/run/mysqld/mysqld.sock",  # mac + darwinports + mysql
+              "/opt/local/var/run/mysql4/mysqld.sock",  # mac + darwinports + mysql4
+              "/opt/local/var/run/mysql5/mysqld.sock",  # mac + darwinports + mysql5
+              "/opt/lampp/var/mysql/mysql.sock"         # xampp for linux
+            ].find { |f| File.exist?(f) }
+          end
+        end
+      end
+    end
+  end
+end
