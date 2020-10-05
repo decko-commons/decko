0346ee6fa6f3f4b96b2edf3cd5f44b8fbab4ff96
Ethan McCutchen <ethan@grasscommons.org>
2020-10-05 11:33:34 -0400

    Semaphore2: expand DECKO_REPO_PATH in submodules script

--- .semaphore/submodules.rb
+++ .semaphore/submodules.rb
@@ -1,14 +1,21 @@
 #!/usr/bin/env ruby
 
+VERB = ARGV.shift || fail("verb required")
+STATUS_FILE = File.expand_path "#{ENV["DECKO_REPO_PATH"]}/substat.txt"
+STATUS_REGEXP = %r{^.(?<sha>\S*) (?<path>\S*)}
+
 # Loop through each git submodule and handle semaphore caching
 
 # Note, this was previously attempted with `git submodule foreach`, but that
-# only works when
+# commands assumes submodules have already been cloned with `git submodule update`.
+# The point of this optimization is to prevent cloning, so instead we use
+# `git submodule status` to produce a status file (one that is also used in other
+# semaphore job commands), and then parse that file to get the sha and path we
+# need for caching.
 
-VERB = ARGV.shift || fail("verb required")
-system %(git submodule > substat.txt)
+system %(git submodule status > #{STATUS_FILE})
 
-File.read("#{ENV["DECKO_REPO_PATH"]}/substat.txt").split("\n") do |line|
-  hash = line.match(/^.(?<sha>\S*) (?<path>\S*)/)
+File.read(STATUS_FILE).split("\n") do |line|
+  hash = line.match STATUS_REGEXP
   system "cache #{VERB} git-submodule-#{hash[:sha]} #{hash[:path] if VERB == 'store'}"
 end
