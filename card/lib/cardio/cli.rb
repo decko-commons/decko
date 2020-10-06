da0cca4823588da8cc649f4a6beeaca3838caa85
Gerry Gleason <gerrygleason5700@gmail.com>
2020-10-06 06:00:46 -0500

    Move card commands to right targets

---
+++ card/lib/cardio/cli.rb
@@ -0,0 +1,17 @@
+require "rbconfig"
+require "cardio/commands/application"
+require "cardio/script_loader"
+
+# If we are inside a Card application this method performs an exec and thus
+# the rest of this script is not run.
+Cardio::ScriptLoader.exec_script! :card
+
+require "rails/ruby_version_check"
+Signal.trap("INT") { puts; exit(1) }
+
+# if ARGV.first == 'plugin'
+#  ARGV.shift
+#  require 'cardio/commands/plugin_new'
+# else
+
+# end
