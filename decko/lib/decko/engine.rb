128d1f0155bbaef95da8998951190f6b9a4df801
Gerry Gleason <gerrygleason5700@gmail.com>
2020-10-17 14:21:31 -0500

    Fix some constants

--- decko/lib/decko/engine.rb
+++ decko/lib/decko/engine.rb
@@ -24,21 +24,15 @@ module Decko
 
     rake_tasks do
       load_task_dir ::Cardio.gem_root
-warn "DENG PATHS #{__LINE__}"
     end
-warn "DENG PATHS #{__LINE__}"
     load_tasks
 
     rake_tasks do
       load_task_dir ::Decko.gem_root
-warn "DENG PATHS #{__LINE__}"
     end
-warn "DENG PATHS #{__LINE__}"
 
     initializer :set_autoload_paths, group: :all do
       config.autoload_paths = Cardio.config.autoload_paths
-warn "DENG #{__LINE__} #{config.autoload_paths.map(&:to_s)}"
-warn "DENG #{__LINE__} #{Cardio.config.autoload_paths.map(&:to_s)}"
     end
 
     initializer :set_autoload_paths, group: :all do
@@ -48,19 +42,17 @@ warn "DENG #{__LINE__} #{Cardio.config.autoload_paths.map(&:to_s)}"
     initializer before: :set_load_path do
       Rails.autoloaders.main.ignore(File.join(Decko.gem_root, "lib/rails/*-routes.rb"))
 
-warn "DENG PATHS #{__LINE__}"
-      paths.add "app/controllers",  with: "rails/controllers", eager_load: true
-      paths.add "gem-assets",       with: "rails/assets"
+      paths.add "app/controllers",  with: "lib/rails/controllers", eager_load: true
+      paths.add "gem-assets",       with: "lib/rails/assets"
 
-      paths.add "config/routes.rb", with: "rails/engine-routes.rb"
+      paths.add "config/routes.rb", with: "lib/rails/engine-routes.rb"
       unless paths["config/routes.rb"].existent.present?
-        paths.add "config/routes.rb", with: "rails/application-routes.rb"
+        paths.add "config/routes.rb", with: "lib/rails/application-routes.rb"
       end
     end
 
     initializer "decko.engine.load_config_initializers",
                 after: :load_config_initializers do
-warn "DENG INITS #{__LINE__}"
       paths["config/initializers"].existent.sort.each do |initializer|
         load(initializer)
       end
