62a06714a96eb9fbdb755a735a4981557002bf76
Ethan McCutchen <ethan@decko.org>
2020-10-18 16:37:29 -0400

    revert machine_spec

--- card-mod-machines/spec/set/abstract/machine_spec.rb
+++ card-mod-machines/spec/set/abstract/machine_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe Card::Set::Abstract::Machine do
       Card[:all, :script].make_machine_output_coded
       mod_path = Card::Mod.dirs.path "machines"
       path = File.join mod_path, "file", "all_script_machine_output", "file.js"
-      expect(path).to be_exist
+      expect(File.exist?(path)).to be_truthy
     end
   end
 
