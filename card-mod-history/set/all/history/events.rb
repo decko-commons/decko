002f3781c4cfd83e564b5dce4361675be24f4053
Gerry Gleason <gerrygleason5700@gmail.com>
2020-10-20 13:29:05 -0500

    Revert history event change

--- card-mod-history/set/all/history/events.rb
+++ card-mod-history/set/all/history/events.rb
@@ -32,11 +32,7 @@ end
 # removes the action if there are no changes
 event :finalize_action, :finalize, when: :finalize_action? do
   if changed_fields.present?
-    if @current_action.new_record?
-      @current_action.card_id = id
-    else
-      @current_action.update! card_id: id
-    end
+    @current_action.update! card_id: id
 
     # Note: #last_change_on uses the id to sort by date
     # so the changes for the create changes have to be created before the first change
