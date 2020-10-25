9c1fe0dea785c0502ddd8a5ca03c28ec4c858715
Ethan McCutchen <ethan@decko.org>
2020-10-24 23:35:18 -0400

    navbar_links_spec duplicate code fix

--- card-mod-layout/spec/set/all/navbar_links_spec.rb
+++ card-mod-layout/spec/set/all/navbar_links_spec.rb
@@ -1,9 +1,8 @@
 RSpec.describe Card::Set::All::NavbarLinks do
   describe "view: navbar_links" do
-    it "renders pointer as dropdown" do
-      content = "[[Stacks|stacky]]\n[[:recent|Recent]]"
+    def expect_navbar_link content, divider=false
       expect_view(:navbar_links, card: { name: "B", type: :pointer, content: content })
-        .to have_tag :ul do
+          .to have_tag :ul do
         with_tag "li.nav-item.dropdown" do
           with_tag "a.nav-link.dropdown-toggle", "stacky"
           with_tag "div.dropdown-menu" do
@@ -11,45 +10,24 @@ RSpec.describe Card::Set::All::NavbarLinks do
             with_tag "a.dropdown-item", "horizontal"
           end
         end
+        with_tag "div.dropdown-divider" if divider
         with_tag "li.nav-item" do
-          with_tag "a.nav-link", "Recent"
+          with_tag "a.nav-link", yield
         end
       end
     end
 
+    it "renders pointer as dropdown" do
+      expect_navbar_link("[[Stacks|stacky]]\n[[:recent|Recent]]") { "Recent" }
+    end
+
     it "handles nest" do
-      content = "{{Stacks|title: stacky}}\n[[A|link to A]]"
-      expect_view(:navbar_links, card: { name: "B", type: :pointer, content: content })
-        .to have_tag :ul do
-        with_tag "li.nav-item.dropdown" do
-          with_tag "a.nav-link.dropdown-toggle", "stacky"
-          with_tag "div.dropdown-menu" do
-            with_tag "a.dropdown-item", "vertical"
-            with_tag "a.dropdown-item", "horizontal"
-          end
-        end
-        with_tag "li.nav-item" do
-          with_tag "a.nav-link", "link to A"
-        end
-      end
+      expect_navbar_link("{{Stacks|title: stacky}}\n[[A|link to A]]") { "link to A" }
     end
 
     example "divider and explicit link" do
       content = "[[Stacks|stacky]]\n[[*dropdown divider]]\n[[http://google.com|Google]]"
-      expect_view(:navbar_links, card: { name: "B", type: :pointer, content: content })
-        .to have_tag :ul do
-        with_tag "li.nav-item.dropdown" do
-          with_tag "a.nav-link.dropdown-toggle", "stacky"
-          with_tag "div.dropdown-menu" do
-            with_tag "a.dropdown-item", "vertical"
-            with_tag "a.dropdown-item", "horizontal"
-          end
-        end
-        with_tag "div.dropdown-divider"
-        with_tag "li.nav-item" do
-          with_tag "a.nav-link", "Google"
-        end
-      end
+      expect_navbar_link(content, true) { "Google" }
     end
   end
 end
