4498b7cdbb9669f16ccef19f12b8d5691cab3872
Ethan McCutchen <ethan@decko.org>
2020-12-18 21:02:52 -0500

    another attempt to fix load strategy testing issue

--- card/spec/cardio/mod/loader/set_loader_spec.rb
+++ card/spec/cardio/mod/loader/set_loader_spec.rb
@@ -6,15 +6,26 @@ RSpec.describe Cardio::Mod::Loader::SetLoader do
     Cardio::Mod::Dirs.new path
   end
 
+  def resetting_load_strategy
+    load_strategy = Cardio::Mod::LoadStrategy.current
+    yield
+    Cardio::Mod::LoadStrategy.current = load_strategy
+  end
+
   it 'initializes the load strategy' do
-    expect(Cardio::Mod::LoadStrategy::Eval).to receive(:new).with(mod_dirs, instance_of(described_class))
-    described_class.new(load_strategy: :eval, mod_dirs: mod_dirs)
+    resetting_load_strategy do
+      expect(Cardio::Mod::LoadStrategy::Eval)
+          .to receive(:new).with(mod_dirs, instance_of(described_class))
+      described_class.new(load_strategy: :eval, mod_dirs: mod_dirs)
+    end
   end
 
   it "load mods" do
-    described_class.new(load_strategy: :eval, mod_dirs: mod_dirs).load
-    expect(Card::Set.const_defined?("All::TestSet")).to be_truthy
-    Card::Set.process_base_modules
-    expect(Card.take.test_method).to eq "method works"
+    resetting_load_strategy do
+      described_class.new(load_strategy: :eval, mod_dirs: mod_dirs).load
+      expect(Card::Set.const_defined?("All::TestSet")).to be_truthy
+      Card::Set.process_base_modules
+      expect(Card.take.test_method).to eq "method works"
+    end
   end
 end
