23f8ec3ef73e79409c232200d5e57830a875d0ca
Ethan McCutchen <ethan@decko.org>
2021-03-28 00:44:05 -0400

    reduce duplication in contextual_spec.rb

--- cardname/spec/lib/cardname/contextual_spec.rb
+++ cardname/spec/lib/cardname/contextual_spec.rb
@@ -1,47 +1,56 @@
 # encoding: utf-8
+
 require_relative "../../spec_helper"
 
 RSpec.describe Cardname::Contextual do
+  def expect_absolute name, context
+    expect name.to_name.absolute(context)
+  end
+
+  def expect_from name, *from
+    expect name.to_name.from(*from)
+  end
+
   describe "#absolute" do
     it "handles _self, _whole, _" do
-      expect("_self".to_name.absolute("foo")).to eq("foo")
-      expect("_whole".to_name.absolute("foo")).to eq("foo")
-      expect("_".to_name.absolute("foo")).to eq("foo")
+      expect_absolute("_self",  "foo").to eq("foo")
+      expect_absolute("_whole", "foo").to eq("foo")
+      expect_absolute("_",      "foo").to eq("foo")
     end
 
     it "handles _left" do
-      expect("_left+Z".to_name.absolute("A+B+C")).to eq("A+B+Z")
+      expect_absolute("_left+Z", "A+B+C").to eq("A+B+Z")
     end
 
     it "handles white space" do
-      expect("_left + Z".to_name.absolute("A+B+C")).to eq("A+B+Z")
+      expect_absolute("_left + Z", "A+B+C").to eq("A+B+Z")
     end
 
     it "handles _right" do
-      expect("_right+bang".to_name.absolute("nutter+butter")).to eq("butter+bang")
-      expect("C+_right".to_name.absolute("B+A")).to eq("C+A")
+      expect_absolute("_right+bang", "nutter+butter").to eq("butter+bang")
+      expect_absolute("C+_right", "B+A").to eq("C+A")
     end
 
     it "handles leading +" do
-      expect("+bug".to_name.absolute("hum")).to eq("hum+bug")
+      expect_absolute("+bug", "hum").to eq("hum+bug")
     end
 
     it "handles trailing +" do
-      expect("bug+".to_name.absolute("tracks")).to eq("bug+tracks")
+      expect_absolute("bug+", "tracks").to eq("bug+tracks")
     end
 
     it "handles leading + in context" do
-      expect("+B".to_name.absolute("+A")).to eq("+A+B")
+      expect_absolute("+B", "+A").to eq("+A+B")
     end
 
     it "handles leading + in context for child" do
-      expect("+A+B".to_name.absolute("+A")).to eq("+A+B")
+      expect_absolute("+A+B", "+A").to eq("+A+B")
     end
 
     it "handles _(numbers)" do
-      expect("_1".to_name.absolute("A+B+C")).to eq("A")
-      expect("_1+_2".to_name.absolute("A+B+C")).to eq("A+B")
-      expect("_2+_3".to_name.absolute("A+B+C")).to eq("B+C")
+      expect_absolute("_1",    "A+B+C").to eq("A")
+      expect_absolute("_1+_2", "A+B+C").to eq("A+B")
+      expect_absolute("_2+_3", "A+B+C").to eq("B+C")
     end
 
     it "handles empty name" do
@@ -49,51 +58,51 @@ RSpec.describe Cardname::Contextual do
     end
 
     it "handles _LLR etc" do
-      expect("_R".to_name.absolute("A+B+C+D+E")).to    eq("E")
-      expect("_L".to_name.absolute("A+B+C+D+E")).to    eq("A+B+C+D")
-      expect("_LR".to_name.absolute("A+B+C+D+E")).to   eq("D")
-      expect("_LL".to_name.absolute("A+B+C+D+E")).to   eq("A+B+C")
-      expect("_LLR".to_name.absolute("A+B+C+D+E")).to  eq("C")
-      expect("_LLL".to_name.absolute("A+B+C+D+E")).to  eq("A+B")
-      expect("_LLLR".to_name.absolute("A+B+C+D+E")).to eq("B")
-      expect("_LLLL".to_name.absolute("A+B+C+D+E")).to eq("A")
+      expect_absolute("_R", "A+B+C+D+E").to    eq("E")
+      expect_absolute("_L", "A+B+C+D+E").to    eq("A+B+C+D")
+      expect_absolute("_LR", "A+B+C+D+E").to   eq("D")
+      expect_absolute("_LL", "A+B+C+D+E").to   eq("A+B+C")
+      expect_absolute("_LLR", "A+B+C+D+E").to  eq("C")
+      expect_absolute("_LLL", "A+B+C+D+E").to  eq("A+B")
+      expect_absolute("_LLLR", "A+B+C+D+E").to eq("B")
+      expect_absolute("_LLLL", "A+B+C+D+E").to eq("A")
     end
 
     context "mismatched requests" do
       it "returns _self for _left or _right on simple cards" do
-        expect("_left+Z".to_name.absolute("A")).to eq("A+Z")
-        expect("_right+Z".to_name.absolute("A")).to eq("A+Z")
+        expect_absolute("_left+Z", "A").to eq("A+Z")
+        expect_absolute("_right+Z", "A").to eq("A+Z")
       end
 
       it "handles bogus numbers" do
-        expect("_1".to_name.absolute("A")).to eq("A")
-        expect("_1+_2".to_name.absolute("A")).to eq("A+A")
-        expect("_2+_3".to_name.absolute("A")).to eq("A+A")
+        expect_absolute("_1", "A").to eq("A")
+        expect_absolute("_1+_2", "A").to eq("A+A")
+        expect_absolute("_2+_3", "A").to eq("A+A")
       end
 
       it "handles bogus _llr requests" do
-        expect("_R".to_name.absolute("A")).to eq("A")
-        expect("_L".to_name.absolute("A")).to eq("A")
-        expect("_LR".to_name.absolute("A")).to eq("A")
-        expect("_LL".to_name.absolute("A")).to eq("A")
-        expect("_LLR".to_name.absolute("A")).to eq("A")
-        expect("_LLL".to_name.absolute("A")).to eq("A")
-        expect("_LLLR".to_name.absolute("A")).to eq("A")
-        expect("_LLLL".to_name.absolute("A")).to eq("A")
+        expect_absolute("_R", "A").to eq("A")
+        expect_absolute("_L", "A").to eq("A")
+        expect_absolute("_LR", "A").to eq("A")
+        expect_absolute("_LL", "A").to eq("A")
+        expect_absolute("_LLR", "A").to eq("A")
+        expect_absolute("_LLL", "A").to eq("A")
+        expect_absolute("_LLLR", "A").to eq("A")
+        expect_absolute("_LLLL", "A").to eq("A")
       end
     end
   end
 
   describe "#from" do
     it "ignores ignorables" do
-      expect("you+awe".to_name.from("you")).to eq("+awe")
+      expect_from("you+awe", "you").to eq("+awe")
       #expect("me+you+awe".to_name.from("you")).to eq("me+awe") #HMMM..... what should this do?
-      expect("me+you+awe".to_name.from("me" )).to eq("+you+awe")
-      expect("me+you+awe".to_name.from("me","you")).to eq("+awe")
-      expect("me+you".to_name.from("me","you")).to eq("me+you")
-      expect("?a?+awe".to_name.from("A")).to eq("+awe")
-      expect("+awe".to_name.from()).to eq("+awe")
-      expect("+awe".to_name.from(nil)).to eq("+awe")
+      expect_from("me+you+awe", "me").to eq("+you+awe")
+      expect_from("me+you+awe", "me", "you").to eq("+awe")
+      expect_from("me+you","me", "you").to eq("me+you")
+      expect_from("?a?+awe", "A").to eq("+awe")
+      expect_from("+awe").to eq("+awe")
+      expect_from("+awe", nil).to eq("+awe")
     end
   end
 
