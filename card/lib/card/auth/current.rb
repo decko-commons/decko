b4f563efe66d8a98a541d982878a1096c55b191e
Ethan McCutchen <ethan@decko.org>
2021-04-27 11:53:10 -0400

    reorganizing auth code to separate out apikey/token handling

--- card/lib/card/auth/current.rb
+++ card/lib/card/auth/current.rb
@@ -2,11 +2,38 @@ class Card
   module Auth
     # methods for setting current account
     module Current
+      # id of current user card.
+      # @return [Integer]
+      def current_id
+        @current_id ||= Card::AnonymousID
+      end
+
+      # set the id of the current user.
+      def current_id= card_id
+        @current = @as_id = @as_card = @current_roles = nil
+        card_id = card_id.to_i if card_id.present?
+        @current_id = card_id
+      end
+
+      # current accounted card (must have +\*account)
+      # @return [Card]
+      def current_user
+        return @current if @current&.id == current_id
+
+        @current = Card[current_id]
+      end
+      alias_method :current_card, :current
+
+      def current_roles
+        @current_roles ||= [Card.fetch_name(:anyone_signed_in),
+                            current.fetch(:roles)&.item_names].flatten.compact
+      end
+
       # set current user in process and session
       def signin cardish
         signin_id = Card.id(cardish) || Card::AnonymousID
         self.current_id = signin_id
-        set_session_user signin_id
+        session[session_user_key] = card_id
       end
 
       # current user is not anonymous
@@ -15,55 +42,16 @@ class Card
         current_id != Card::AnonymousID
       end
 
-      # id of current user card.
-      # @return [Integer]
-      def current_id
-        @current_id ||= Card::AnonymousID
+      # set current from token, api_key, or session
+      def signin_with _opts={}
+        signin_with_session
       end
 
-      # current accounted card (must have +\*account)
-      # @return [Card]
-      def current
-        if @current && @current.id == current_id
-          @current
-        else
-          @current = Card[current_id]
-        end
-      end
-
-      def current_roles
-        @current_roles ||= [Card.fetch_name(:anyone_signed_in),
-                            current.fetch(:roles)&.item_names].flatten.compact
-      end
-
-      def serialize
-        { as_id: as_id, current_id: current_id }
-      end
-
-      # @param auth_data [Integer|Hash] user id, user name, or a hash
-      # @option auth_data [Integer] current_id
-      # @option auth_data [Integer] as_id
-      def with auth_data
-        if auth_data.is_a?(Integer) || auth_data.is_a?(String)
-          auth_data = { current_id: Card.id(auth_data) }
-        end
-
-        tmp_current_id = current_id
-        tmp_as_id = as_id
-        tmp_current = @current
-        tmp_as_card = @as_card
-        tmp_current_roles = @current_roles
-
-        # resets @as and @as_card
-        self.current_id = auth_data[:current_id]
-        @as_id = auth_data[:as_id] if auth_data[:as_id]
-        yield
-      ensure
-        @current_id = tmp_current_id
-        @as_id = tmp_as_id
-        @current = tmp_current
-        @as_card = tmp_as_card
-        @current_roles = tmp_current_roles
+      # get :user id from session and set Auth.current_id
+      def signin_with_session
+        card_id = session[session_user_key]
+        card_id = nil unless Card.exists? card_id
+        signin card_id
       end
 
       # get session object from Env
@@ -72,51 +60,17 @@ class Card
         Card::Env.session
       end
 
-      # set current from token, api_key, or session
-      def signin_with opts={}
-        if opts[:token]
-          signin_with_token opts[:token]
-        elsif opts[:api_key]
-          signin_with_api_key opts[:api_key]
-        else
-          signin_with_session
-        end
-      end
-
-      # set the current user based on token
-      def signin_with_token token
-        payload = Token.validate! token
-        signin payload[:anonymous] ? Card::AnonymousID : payload[:user_id]
-      end
-
-      # set the current user based on api_key
-      def signin_with_api_key api_key
-        account = find_account_by_api_key api_key
-        unless account&.validate_api_key! api_key
-          raise Card::Error::PermissionDenied, "API key authentication failed"
-        end
-
-        signin account.left_id
-      end
-
-      # get :user id from session and set Auth.current_id
-      def signin_with_session
-        card_id = session_user
-        signin(card_id && Card.exists?(card_id) ? card_id : nil)
-      end
-
-      # find +\*account card by +\*api card
-      # @param api_key [String]
-      # @return [+*account card, nil]
-      def find_account_by_api_key api_key
-        find_account_by :api_key, api_key.strip
-      end
-
       # find +\*account card by +\*email card
       # @param email [String]
       # @return [+*account card, nil]
       def find_account_by_email email
         find_account_by :email, email.strip.downcase
+      end
+
+      private
+
+      def session_user_key
+        "user_#{Cardio.database_name.underscore}".to_sym
       end
 
       # general pattern for finding +\*account card based on field cards
@@ -129,25 +83,6 @@ class Card
                         right_plus: [Card::Codename.id(fieldcode), { content: value }] },
                       "find +:account with +#{fieldcode} (#{value})").first
         end
-      end
-
-      def session_user
-        session[session_user_key]
-      end
-
-      def set_session_user card_id
-        session[session_user_key] = card_id
-      end
-
-      def session_user_key
-        "user_#{Cardio.database_name.underscore}".to_sym
-      end
-
-      # set the id of the current user.
-      def current_id= card_id
-        @current = @as_id = @as_card = @current_roles = nil
-        card_id = card_id.to_i if card_id.present?
-        @current_id = card_id
       end
     end
   end
