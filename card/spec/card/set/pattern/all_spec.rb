3b18cf6feab89e547fe63e2fc44b05f2887eeffa
Ethan McCutchen <ethan@decko.org>
2021-05-01 20:11:16 -0400

    Pattern::All cleanup

--- card/spec/card/set/pattern/all_spec.rb
+++ card/spec/card/set/pattern/all_spec.rb
@@ -1,6 +1,6 @@
 # -*- encoding : utf-8 -*-
 
-RSpec.describe Card::Set::All::Pattern do
+RSpec.describe Card::Set::Pattern::All do
   describe :set_names do
     it "returns self, type, all for simple cards" do
       Card::Auth.as_bot do
@@ -14,41 +14,41 @@ RSpec.describe Card::Set::All::Pattern do
 
     it "returns set names for simple star cards" do
       Card::Auth.as_bot do
-        expect(Card.fetch("*update").set_names).to eq(
-          [
-            "*update+*self", "*star", "Setting+*type", "*all"
-          ]
-        )
+        expect(Card.fetch("*update").set_names)
+          .to eq(["*update+*self", "*star", "Setting+*type", "*all"])
       end
     end
 
     it "returns set names for junction cards" do
       Card::Auth.as_bot do
-        expect(Card.new(name: "Iliad+author").set_names).to eq(
-          [
-            "Book+author+*type plus right", "author+*right", "RichText+*type",
-            "*all plus", "*all"
-          ]
-        )
+        expect(Card.new(name: "Iliad+author").set_names)
+          .to eq(["Book+author+*type plus right",
+                  "author+*right",
+                  "RichText+*type",
+                  "*all plus",
+                  "*all" ])
       end
     end
 
     it "returns set names for compound star cards", as_bot: true do
-      expect(Card.new(name: "Iliad+*to").set_names).to eq(
-        [
-          "Book+*to+*type plus right", "*to+*right", "*rstar",
-          "List+*type", "*all plus", "*all"
-        ]
-      )
+      expect(Card.new(name: "Iliad+*to").set_names)
+        .to eq(["Book+*to+*type plus right",
+                "*to+*right",
+                "*rstar",
+                "List+*type",
+                "*all plus",
+                "*all"])
     end
 
     it "returns set names for rule cards", as_bot: true do
-      expect(Card.new(name: "*all+*create").set_names).to eq(
-        [
-          "Set+*create+*type plus right", "*create+*right", "*rule", "*rstar",
-          "List+*type", "*all plus", "*all"
-        ]
-      )
+      expect(Card.new(name: "*all+*create").set_names)
+        .to eq(["Set+*create+*type plus right",
+                "*create+*right",
+                "*rule",
+                "*rstar",
+                "List+*type",
+                "*all plus",
+                "*all" ])
     end
 
     # right place for this?  really need more prototype tests...
@@ -80,15 +80,13 @@ RSpec.describe Card::Set::All::Pattern do
 
     it "returns set names for junction cards" do
       card = Card.new(name: "Iliad+author")
-      expect(card.safe_set_keys).to eq(
-        "ALL ALL_PLUS TYPE-rich_text RIGHT-author TYPE_PLUS_RIGHT-book-author"
-      )
+      expect(card.safe_set_keys)
+        .to eq("ALL ALL_PLUS TYPE-rich_text RIGHT-author TYPE_PLUS_RIGHT-book-author")
       card.save!
       card = Card.fetch("Iliad+author")
-      expect(card.safe_set_keys).to eq(
-        "ALL ALL_PLUS TYPE-rich_text RIGHT-author TYPE_PLUS_RIGHT-book-author "\
-        "SELF-iliad-author"
-      )
+      expect(card.safe_set_keys)
+        .to eq("ALL ALL_PLUS TYPE-rich_text RIGHT-author TYPE_PLUS_RIGHT-book-author "\
+               "SELF-iliad-author")
     end
   end
 
