545b5d2fc474f4d049719e4b12c9a1e5d2060511
Ethan McCutchen <ethan@decko.org>
2021-05-14 14:53:40 -0400

    moved pointer specs in from wikirate

--- card-mod-list/spec/set/abstract/pointer/other_views_spec.rb
+++ card-mod-list/spec/set/abstract/pointer/other_views_spec.rb
@@ -38,4 +38,127 @@ RSpec.describe Card::Set::Abstract::Pointer do
       expect(css_list).to match(/#{Regexp.escape css}/)
     end
   end
+
+  describe "rendering json in export mode" do
+    let(:elbert) do
+      create "Elbert Hubbard", content: "Do not take life too seriously."
+    end
+    let(:elbert_punchline) do
+      create "Elbert Hubbard+punchline", content: "You will never get out of it alive."
+    end
+    let(:elbert_quote) do
+      create "Elbert Hubbard+quote",
+             content: "Procrastination is the art of keeping up with yesterday."
+    end
+    let(:elbert_container) do
+      create "elbert container", type_id: Card::PointerID, content: "[[#{elbert.name}]]"
+    end
+
+    def json_export args={}
+      @json ||= begin
+                  args[:name] ||= "normal pointer"
+                  args[:type] ||= :pointer
+                  if args[:content].is_a?(Array)
+                    args[:content] = args[:content].to_pointer_content
+                  end
+                  Card::Auth.as_bot do
+                    collection_card = Card.create! args
+                    card = Card.create! name: "export card",
+                                        type_id: Card::PointerID,
+                                        content: "[[#{collection_card.name}]]"
+                    card.format(:json).render_export
+                  end
+                end
+    end
+
+    context "pointer card" do
+      it "contains cards in the pointer card and its children" do
+        Card::Env.params[:max_export_depth] = 3
+        json_export type: :pointer, content: [elbert.name, elbert_punchline.name]
+
+        expect(json_export)
+          .to include(
+            a_hash_including(name: "normal pointer",
+                             type: "Pointer",
+                             content: "[[Elbert Hubbard]]\n[[Elbert Hubbard+punchline]]"),
+            a_hash_including(name: "Elbert Hubbard",
+                             type: "RichText",
+                             content: "Do not take life too seriously."),
+            a_hash_including(name: "Elbert Hubbard+punchline",
+                             type: "RichText",
+                             content: "You will never get out of it alive.")
+          )
+      end
+
+      it "handles multi level pointer cards" do
+        Card::Env.params[:max_export_depth] = 4
+        json_export type: :pointer,
+                    content: [elbert_container.name, elbert_punchline.name]
+
+        expect(json_export)
+          .to include(
+            a_hash_including(name: "normal pointer",
+                             type: "Pointer",
+                             content: "[[elbert container]]\n" \
+                                  "[[Elbert Hubbard+punchline]]"),
+            a_hash_including(name: "elbert container",
+                             type: "Pointer",
+                             content: "[[Elbert Hubbard]]"),
+            a_hash_including(name: "Elbert Hubbard",
+                             type: "RichText",
+                             content: "Do not take life too seriously."),
+            a_hash_including(name: "Elbert Hubbard+punchline",
+                             type: "RichText",
+                             content: "You will never get out of it alive.")
+          )
+      end
+
+      it "stops if the depth count > 10" do
+        json_export type: :pointer, name: "normal pointer", content: ["normal pointer"]
+        expect(json_export)
+          .to include(a_hash_including(name: "normal pointer", type: "Pointer",
+                                       content: "[[normal pointer]]"))
+      end
+    end
+
+    context "Skin card" do
+      it "contains cards in the pointer card and its children" do
+        Card::Env.params[:max_export_depth] = 4
+        expect(json_export(type: :skin, content: [elbert.name]))
+          .to include(
+            a_hash_including(name: "normal pointer",
+                             type: "Skin",
+                             content: "[[Elbert Hubbard]]"),
+            a_hash_including(name: "Elbert Hubbard",
+                             type: "RichText",
+                             content: "Do not take life too seriously.")
+          )
+      end
+    end
+
+    context "search card" do
+      it "contains cards from search card and its children" do
+        elbert
+        elbert_punchline
+        elbert_quote
+
+        Card::Env.params[:max_export_depth] = 4
+        json_export(name: "search card", type: :search_type,
+                    content: %({"left":"Elbert Hubbard"}))
+        expect(json_export)
+          .to include(
+            a_hash_including(name: "search card",
+                             type: "Search",
+                             content: %({"left":"Elbert Hubbard"})),
+            a_hash_including(name: "Elbert Hubbard+punchline",
+                             type: "RichText",
+                             content: "You will never get out of it alive."),
+            a_hash_including(name: "Elbert Hubbard+quote",
+                             type: "RichText",
+                             content: "Procrastination is " \
+                                  "the art of keeping up with yesterday.")
+          )
+      end
+    end
+  end
 end
