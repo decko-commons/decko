5c8a71971c34903dc861fffc7928fce0233c72cc
Ethan McCutchen <ethan@decko.org>
2021-05-28 16:04:22 -0400

    trying to fix ComponentClass loading bug

--- mod/bootstrap/lib/card/bootstrap/component.rb
+++ mod/bootstrap/lib/card/bootstrap/component.rb
@@ -1,11 +1,8 @@
-require "card/bootstrap/component/component_class"
-# above should not be necessary, but deployments failing without it
-
 class Card
   class Bootstrap
     # render components of bootstrap library
     class Component
-      extend ComponentClass
+      extend ComponentKlass
 
       def initialize context, *args, &block
         @context = context
