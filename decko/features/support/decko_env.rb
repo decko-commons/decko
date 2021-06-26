be9ea02596c62e4eb735852d0ebed1655a0b36b9
Ethan McCutchen <ethan@decko.org>
2021-06-26 15:57:52 -0400

    simplified cucumber callbacks

--- decko/features/support/decko_env.rb
+++ decko/features/support/decko_env.rb
@@ -7,27 +7,27 @@ World(RSpec::Matchers)
 require "rspec-html-matchers"
 World(RSpecHtmlMatchers)
 
-Before("@background-jobs or @delayed-jobs or @javascript") do
+Before do
   Cardio.seed_test_db
 end
 
-Before("not @background-jobs", "not @delayed-jobs", "not @javascript") do
-  DatabaseCleaner.strategy = :transaction
-  DatabaseCleaner.start
-end
-
-After("not @background-jobs", "not @delayed-jobs", "not @javascript") do
-  DatabaseCleaner.clean
-end
+# Before("not @background-jobs", "not @delayed-jobs", "not @javascript") do
+#   DatabaseCleaner.strategy = :transaction
+#   DatabaseCleaner.start
+# end
+#
+# After("not @background-jobs", "not @delayed-jobs", "not @javascript") do
+#   DatabaseCleaner.clean
+# end
 
 at_exit do
   Cardio.seed_test_db
 end
 # frozen_string_literal: true
 
-Before("@javascript") do
-  @javascript = true
-end
+# Before("@javascript") do
+#   @javascript = true
+# end
 
 Capybara.configure do |config|
   config.match = :prefer_exact
@@ -47,6 +47,7 @@ After("@simulate-setup") do
 end
 
 # Capybara.register_server :puma do |app, port, host, options={}|
+#   require "rack/handler/puma"
 #   options.merge! Host: host, Port: port, Threads: "0:1", workers: 0, daemon: false
 #   Rack::Handler::Puma.run app, **options
 # end
