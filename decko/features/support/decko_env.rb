948396f2fe52df2a96bfd429392efcbb41b7869b
Ethan McCutchen <ethan@decko.org>
2021-06-27 22:05:25 -0400

    re-remove unneeded stuff from decko_env.rb

--- decko/features/support/decko_env.rb
+++ decko/features/support/decko_env.rb
@@ -3,31 +3,17 @@
 require "email_spec"
 require "email_spec/cucumber"
 
-World(RSpec::Matchers)
-require "rspec-html-matchers"
-World(RSpecHtmlMatchers)
-
 Before do
   Cardio.seed_test_db
 end
 
-# Before("not @background-jobs", "not @delayed-jobs", "not @javascript") do
-#   DatabaseCleaner.strategy = :transaction
-#   DatabaseCleaner.start
-# end
-#
-# After("not @background-jobs", "not @delayed-jobs", "not @javascript") do
-#   DatabaseCleaner.clean
-# end
-
 at_exit do
   Cardio.seed_test_db
 end
-# frozen_string_literal: true
 
-# Before("@javascript") do
-#   @javascript = true
-# end
+Before("@javascript") do
+  @javascript = true
+end
 
 Capybara.configure do |config|
   config.match = :prefer_exact
