55cdc9ed39500dadab032015bcffcfd5d5a47d6a
Ethan McCutchen <ethan@decko.org>
2021-07-05 14:26:29 -0400

    upgrading google analytics tracking

--- mod/google_analytics/set/all/google_analytics.rb
+++ mod/google_analytics/set/all/google_analytics.rb
@@ -1,32 +1,43 @@
 require "staccato"
 
-mattr_accessor :server_side_tracking_formats
-
-self.server_side_tracking_formats = %i[csv json]
-
-event :track_page, before: :show_page, when: :track_page? do
-  tracker.pageview path: Env.controller.request&.path, host: Env.host, title: name
-end
-
-def track_page?
-  google_analytics_key &&
-    Env.controller&.response_format&.in?(server_side_tracking_formats)
-end
-
-def tracker
-  return unless google_analytics_key
-
-  ::Staccato.tracker google_analytics_key # , nil, ssl: true
+event :track_page, before: :show_page, when: :track_page_from_server? do
+  tracker.pageview tracker_options
 end
 
 def google_analytics_key
   @google_analytics_key ||=
     Card::Rule.global_setting(:google_analytics_key) ||
-    Card.config.google_analytics_key
+      Card.config.google_analytics_key
+end
+
+def tracker
+  tracker_key && ::Staccato.tracker(tracker_key) # , nil, ssl: true
+end
+
+# can override to have separate keys for web and API
+def tracker_key
+  @tracker_key ||= google_analytics_key
+end
+
+def tracker_options
+  r = Env.controller.request
+  {
+    path: r.path,
+    host: Env.host,
+    title: name,
+    uid: Auth.current_id,
+    uip: r.remote_ip,
+    ni: 1, # non-interactive
+  }
+end
+
+# for override
+def track_page_from_server?
+  false
 end
 
 format :html do
-  delegate :tracker, :google_analytics_key, to: :card
+  delegate :google_analytics_key, to: :card
 
   def views_in_head
     super << :google_analytics_snippet
