e2e835289fb6a7548eea8cef59e8464fc8d743a4
Ethan McCutchen <ethan@decko.org>
2021-08-08 23:49:49 -0400

    new loader pattern working

--- card/lib/card/set/pattern.rb
+++ card/lib/card/set/pattern.rb
@@ -7,21 +7,17 @@ class Card
         end
 
         def reset
-          nonbase_loadables.each do |set_pattern|
+          reloadables.each do |set_pattern|
             Set.const_remove_if_defined set_pattern.to_s.split("::").last
           end
           @concrete = []
           @card_keys = @codes = @nonbase_codes = @ids = nil
         end
 
-        def loadables
-          concrete.push(Card::Set::Abstract).reverse
-        end
-
-        def nonbase_loadables
-          l = loadables
-          l.delete Set::All
-          l
+        def reloadables
+          r = concrete.push(Abstract)
+          r.delete Set::All
+          r
         end
 
         def find pattern_code
@@ -35,16 +31,24 @@ class Card
             end
         end
 
-        def nonbase_codes
-          @nonbase_codes ||= codes.tap { |list| list.delete :all }
-        end
-
-        def codes
-          @codes ||= concrete.map(&:pattern_code).push(:abstract).reverse
+        def grouped_codes with_all: true
+          g = [[:abstract], nonbase_codes.reverse]
+          g.unshift [:all] if with_all
+          g
         end
 
         def ids
           @ids ||= concrete.map(&:pattern_id)
+        end
+
+        private
+
+        def codes
+          @codes ||= concrete.map(&:pattern_code)
+        end
+
+        def nonbase_codes
+          @nonbase_codes ||= codes.tap { |list| list.delete :all }
         end
       end
     end
