9e94f09fa077f4d3ea8478179119bd51d272fa79
Ethan McCutchen <ethan@decko.org>
2021-10-15 00:07:03 -0400

    lame spec fix attempt

--- mod/carrierwave/lib/carrier_wave/file_card_uploader.rb
+++ mod/carrierwave/lib/carrier_wave/file_card_uploader.rb
@@ -212,6 +212,10 @@ module CarrierWave
         action_id_stand_in
     end
 
+    def reset_action_id
+      @action_id = nil
+    end
+
     # delegate carrierwave's fog config methods to bucket configuration
     CONFIG_OPTIONS.each do |name|
       define_method("fog_#{name}") { bucket_config name }
--- mod/carrierwave/spec/set/abstract/attachment/cloud_spec.rb
+++ mod/carrierwave/spec/set/abstract/attachment/cloud_spec.rb
@@ -43,6 +43,7 @@ RSpec.describe Card::Set::Abstract::Attachment::Cloud do
         # local
         expect(file_card.db_content).to eq "~#{file_path}"
 
+        file_card.file.reset_action_id
         # cloud
         file_card.update! storage_type: :cloud
         expect(file_card.db_content).to eq("(test_bucket)/#{file_path}")
