a1b48b62f884065f80390e6e5dd9ee99b516ba87
Ethan McCutchen <ethan@decko.org>
2021-11-07 12:36:34 -0500

    migrate lists

---
+++ mod/list/db/migrate_core_cards/20211107155032_out_with_the_brackets.rb
@@ -0,0 +1,13 @@
+# -*- encoding : utf-8 -*-
+
+# delete brackets from card content
+class OutWithTheBrackets < Cardio::Migration
+  def up
+    list_type_ids = %i[pointer list].map(&:card_id)
+    ["[[", "]]"].each do |brackets|
+      Card.where(type_id: list_type_ids).in_batches.update_all(
+        db_content: "REPLACE(db_content, '#{brackets}', '')"
+      )
+    end
+  end
+end
