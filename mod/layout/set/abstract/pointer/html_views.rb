be869fd861dc90f329a9f25a8c1936c8da282f40
Ethan McCutchen <ethan@decko.org>
2021-11-07 14:56:24 -0500

    fix specs that brackletness broke

--- mod/layout/set/abstract/pointer/html_views.rb
+++ mod/layout/set/abstract/pointer/html_views.rb
@@ -1,4 +1,11 @@
 format :html do
+  def navbar_items view: :nav_item, link_class: nil
+    card.item_cards.map do |item_card|
+      item = nest item_card, view: view
+      view == :nav_item ? wrap_with_nav_item(item) : item
+    end
+  end
+
   view :nav_item do
     nav_dropdown
   end
