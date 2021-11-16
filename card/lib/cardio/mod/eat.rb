40868650708838fd10488c3d1060fa31d4c5e486
Ethan McCutchen <ethan@decko.org>
2021-11-16 16:40:44 -0500

    edibles refactor

--- card/lib/cardio/mod/eat.rb
+++ card/lib/cardio/mod/eat.rb
@@ -9,6 +9,7 @@ module Cardio
     # https://docs.google.com/document/d/13K_ynFwfpHwc3t5gnLeAkZJZHco1wK063nJNYwU8qfc/edit#
     class Eat
       include Card::Model::SaveHelper
+      include Edibles
 
       def initialize mod: nil, env: nil, user: nil, verbose: nil
         @mod = mod
@@ -21,36 +22,29 @@ module Cardio
         Card::Cache.reset_all
         Card::Mailer.perform_deliveries = false
         Card::Auth.as_bot do
-          items.each do |item|
+          edibles.each do |edible|
             track do
-              current_user item.delete(:user)
-              time_machine item.delete(:time) do
-                ensure_card item
+              current_user edible.delete(:user)
+              time_machine edible.delete(:time) do
+                ensure_card edible
               end
             end
           end
         end
       end
 
-      # list of card attribute hashes
-      # @return [Array <Hash>]
-      def items
-        mods_with_data.map { |mod| mod_items mod }.flatten
-      end
-
       private
 
-      # @return [Array <Cardio::Mod>]
-      def mods_with_data
-        paths = Mod.dirs.subpaths "data"
-        mod_names = @mod ? ensure_mod_data_path(paths) : paths.keys
-        mod_names.map { |mod_name| Mod.fetch mod_name }
+      def track
+        card = yield
+        puts "eaten: #{card.name}".green if @verbose
+      rescue StandardError => e
+        puts e.message.red
+        puts e.backtrace.join("\n") if @verbose
       end
 
-      def ensure_mod_data_path paths
-        return [@mod] if paths[@mod]
-
-        raise "no data directory found for mod #{@mod}".red
+      def current_user item_user
+        Card::Auth.current_id = item_user&.card_id || @user_id || Card::WagnBotID
       end
 
       def time_machine value, &block
@@ -69,81 +63,6 @@ module Cardio
         else
           raise TypeError, "invalid time value: #{value}. accepts int, +int, and -int"
         end
-      end
-
-      def current_user item_user
-        Card::Auth.current_id = item_user&.card_id || @user_id || Card::WagnBotID
-      end
-
-      def track
-        card = yield
-        puts "eaten: #{card.name}".green if @verbose
-      rescue StandardError => e
-        puts e.message.red
-        puts e.backtrace.join("\n") if @verbose
-      end
-
-      # @return [Array <Hash>]
-      def mod_items mod
-        environments.map { |env| items_for_environment mod, env }.compact
-      end
-
-      def items_for_environment mod, env
-        return unless (items = items_from_yaml mod, env)
-
-        if items.first.is_a? String
-          items.map! { |filename| items_from_yaml mod, env, filename }
-        end
-        interpret_items mod, items
-      end
-
-      def interpret_items mod, items
-        each_card_hash(items) { |hash| handle_attachments mod, hash }
-      end
-
-      def items_from_yaml mod, env, filename=nil
-        source = "#{env}#{'/' if filename.present?}#{filename}.yml"
-        return unless (path = mod.subpath "data", source)
-
-        YAML.load_file path
-      end
-
-      def each_card_hash items
-        items.each do |item|
-          yield item
-          item[:subfields]&.values&.each { |val| yield val if val.is_a? Hash }
-        end
-        items
-      end
-
-      def handle_attachments mod, hash
-        attachments hash do |key, filename|
-          hash[key] = mod_file mod, filename
-          hash[:mod] = mod.name if hash[:storage_type] == :coded
-        end
-      end
-
-      def attachments hash
-        attachment_keys.each do |key|
-          yield key, hash[key] if hash.key? key
-        end
-      end
-
-      def mod_file mod, filename
-        File.open mod.subpath("data/files", filename)
-      end
-
-      def attachment_keys
-        @attachment_keys ||= Card.uploaders.keys
-      end
-
-      # @return [Array <Symbol>]
-      # holarchical. each includes the previous
-      # production = [:production],
-      # development = [:production, :development], etc.
-      def environments
-        index = DATA_ENVIRONMENTS.index(@env&.to_sym || Rails.env.to_sym) || -1
-        DATA_ENVIRONMENTS[0..index]
       end
     end
   end
