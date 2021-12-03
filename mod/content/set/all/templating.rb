e5b2873971bf84bb12ca58b0b0d7225943ce454f
Ethan McCutchen <ethan@decko.org>
2021-12-03 10:18:08 -0500

    make some templating methods private

--- mod/content/set/all/templating.rb
+++ mod/content/set/all/templating.rb
@@ -24,27 +24,6 @@ def template
   end
 end
 
-def new_card_template
-  default = rule_card :default, skip_modules: true
-  return default unless (structure = dup_structure default&.type_id)
-
-  @virtual = true if compound?
-  self.type_id = structure.type_id if assign_type_to?(structure)
-  structure
-end
-
-def dup_structure type_id
-  dup_card = dup
-  dup_card.type_id = type_id || Card.default_type_id
-  dup_card.structure_rule_card
-end
-
-def assign_type_to? structure
-  return if type_id == structure.type_id
-
-  structure.assigns_type?
-end
-
 def assigns_type?
   # needed because not all *structure templates govern the type of set members
   # for example, X+*type+*structure governs all cards of type X,
@@ -63,4 +42,25 @@ def structure_rule_card
   return unless (card = rule_card :structure, skip_modules: true)
 
   card.db_content&.strip == "_self" ? nil : card
+end
+
+private
+
+def new_card_template
+  default = rule_card :default, skip_modules: true
+  return default unless (structure = dup_structure default&.type_id)
+
+  @virtual = true if compound?
+  self.type_id = structure.type_id if assign_type_to?(structure)
+  structure
+end
+
+def dup_structure type_id
+  dup_card = dup
+  dup_card.type_id = type_id || Card.default_type_id
+  dup_card.structure_rule_card
+end
+
+def assign_type_to? structure
+  type_id == structure.type_id && structure.assigns_type?
 end
