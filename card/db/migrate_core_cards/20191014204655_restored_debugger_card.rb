c1e23f6bb4bbc7d409209b70b9259595008ccef4
Philipp Kühl <philipp.kuehl@gmail.com>
2021-12-16 22:38:20 +0100

    remove accidentally committed migration file

--- card/db/migrate_core_cards/20191014204655_restored_debugger_card.rb
+++
@@ -1,7 +0,0 @@
-# -*- encoding : utf-8 -*-
-
-class RestoredDebuggerCard < Card::Migration::Core
-  def up
-    ensure_code_card "*debugger", codename: "debugger", type_id: Card::SessionID
-  end
-end
