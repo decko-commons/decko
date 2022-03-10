24b79da37ea6cae4851da2ff48be251e4d0935f1
Philipp Kühl <philipp.kuehl@gmail.com>
2022-03-10 23:19:59 +0100

    autocomplete for nest list

--- mod/search/set/all/autocomplete.rb
+++ mod/search/set/all/autocomplete.rb
@@ -4,4 +4,10 @@ format :html do
          item: item, options_card: options_card_name,
          extra_classes: extra_classes
   end
+
+  def name_autocomplete_field item, extra_classes=""
+    haml :autocomplete_input,
+         item: item, options_card: [:all, :by_name],
+         extra_classes: extra_classes
+  end
 end
