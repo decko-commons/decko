abf71b0d98a2cc10b3e20cfbac334335c964b9bc
Ethan McCutchen <ethan@decko.org>
2022-04-24 15:37:34 -0400

    towards seed:update working...

--- card/db/seeds.rb
+++ card/db/seeds.rb
@@ -2,4 +2,6 @@ require "cardio/seed"
 require "active_record/fixtures"
 
 ActiveRecord::FixtureSet.create_fixtures Cardio::Seed.path, Cardio::Seed::TABLES
-# Cardio::Mod::Loader.load_mods
+
+# TODO: explain why this is necessary
+Cardio::Mod::Loader.load_mods
