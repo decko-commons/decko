de1a6f09f4e2dbfee880434aad1f289eb0ec1c0f
Ethan McCutchen <ethan@decko.org>
2022-04-25 12:15:10 -0400

    another reseed, now with signup fixes

--- card/db/seeds.rb
+++ card/db/seeds.rb
@@ -4,4 +4,5 @@ require "active_record/fixtures"
 ActiveRecord::FixtureSet.create_fixtures Cardio::Seed.path, Cardio::Seed::TABLES
 
 # TODO: explain why this is necessary
+# (card:seed:update breaks without it, but I don't know why)
 Cardio::Mod::Loader.load_mods
