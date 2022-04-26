6cfe855f45b7d629baaa683db81a5bdc86c93c92
Ethan McCutchen <ethan@decko.org>
2022-04-26 14:51:03 -0400

    recaptcha refacta

--- mod/recaptcha/set/self/recaptcha_secret_key.rb
+++
@@ -1,9 +0,0 @@
-event :validate_recaptcha_secret_key, :validate do
-  return if content.match?(/^[a-zA-Z0-9\-_]*$/)
-
-  errors.add :content, "invalid key" # LOCALIZE
-end
-
-event :set_recaptcha_secret_key, :finalize do
-  Card.config.recaptcha_secret_key = content
-end
