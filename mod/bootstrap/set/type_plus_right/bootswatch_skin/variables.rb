2f38a56dd4cb39f7a793aaffcebf167b4ef3ef98
Philipp Kühl <philipp.kuehl@gmail.com>
2022-05-11 00:11:35 +0200

    add link to bootstrap variables in help text

--- mod/bootstrap/set/type_plus_right/bootswatch_skin/variables.rb
+++ mod/bootstrap/set/type_plus_right/bootswatch_skin/variables.rb
@@ -3,3 +3,9 @@ include_set Abstract::SkinField
 def virtual?
   new?
 end
+
+def raw_help_text
+  <<-TEXT
+    Override bootstrap [[https://github.com/twbs/bootstrap/blob/v5.1.3/scss/_variables.scss|variables]]
+  TEXT
+end
