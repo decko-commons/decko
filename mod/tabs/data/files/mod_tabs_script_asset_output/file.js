04b0aae8dd54061dbbb3a8b972135bce64b3cac2
Ethan McCutchen <ethan@decko.org>
2022-05-23 13:29:58 -0400

    more asset refinement

--- mod/tabs/data/files/mod_tabs_script_asset_output/file.js
+++ mod/tabs/data/files/mod_tabs_script_asset_output/file.js
@@ -1,19 +1,2 @@
 // tab.js.coffee
-(function() {
-  $(window).ready(function() {
-    return $('body').on('show.bs.tab', 'a.load[data-bs-toggle="tab"][data-url]', function(e) {
-      var tab_id, url;
-      tab_id = $(e.target).attr('href');
-      url = $(e.target).data('url');
-      $(e.target).removeClass('load');
-      return $.ajax({
-        url: url,
-        success: function(html) {
-          $(tab_id).append(html);
-          return decko.contentLoaded($(tab_id), $(this));
-        }
-      });
-    });
-  });
-
-}).call(this);
+(function(){$(window).ready(function(){return $("body").on("show.bs.tab",'a.load[data-bs-toggle="tab"][data-url]',function(t){var a,r;return a=$(t.target).attr("href"),r=$(t.target).data("url"),$(t.target).removeClass("load"),$.ajax({url:r,success:function(t){return $(a).append(t),decko.contentLoaded($(a),$(this))}})})})}).call(this);
