977da3a613c1113d29674a9ae81f971c15684072
Ethan McCutchen <ethan@decko.org>
2022-05-26 17:33:23 -0400

    script calls basket

--- mod/ace_editor/set/all/ace_editor.rb
+++ mod/ace_editor/set/all/ace_editor.rb
@@ -1,7 +1,11 @@
 basket[:list_input_options] << "ace editor"
-basket[:script_config][:ace] = "setAceConfig"
+basket[:script_calls]["setAceConfig"] = :ace_config
 
 format :html do
+  def ace_config
+    Card::Rule.global_setting :ace
+  end
+
   def ace_editor_input
     text_area :content, rows: 5,
                         class: "d0-card-content ace-editor-textarea",
