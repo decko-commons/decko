f9c7dd565ee1a82a0bfc251c48cd361cbc93bca6
Philipp Kühl <philipp.kuehl@gmail.com>
2022-06-28 23:08:25 +0200

    script and style validation

--- mod/script/spec/set/type/java_script_spec.rb
+++ mod/script/spec/set/type/java_script_spec.rb
@@ -24,6 +24,12 @@ RSpec.describe Card::Set::Type::JavaScript do
       .to have_tag :script, with: { src: "/js_test_card.js" }
   end
 
+  it "validates syntax" do
+    card = Card.create name: "tmp javascript", type_code: "java_script", content: "your turn"
+    expect(card.errors[:content].first)
+      .to include("Unexpected token")
+  end
+
   # script outputters can't be changed with cards
   # it_behaves_like "asset inputter", that_produces: :js  do
   #   let(:create_asset_inputter_card) { js_card }
--- mod/style/set/type/scss.rb
+++ mod/style/set/type/scss.rb
@@ -1,2 +1,21 @@
 include_set Abstract::Scss
 include_set Abstract::AssetInputter, input_format: :scss
+
+event :validate_scss_syntax, :validate, on: :save, changed: %i[type_id content] do
+  variables = Card[:all, :style].joined_items_content
+  SassC::Engine.new([variables.strip, content].join "\n").render
+rescue SassC::SyntaxError => e
+  match = e.message.match(/line (\d+)/)
+  message =
+    if match
+      offset = 6
+      corrected_line = match[1].to_i - variables.lines.count + offset
+      e.message
+       .sub(/line \d+:(\d+) of stdin/, "line #{corrected_line}:\\1")
+       .sub(/>>.*$/, ">> #{content.lines[corrected_line - 1]}")
+      # e.message
+    else
+      e.message
+    end
+  errors.add(:content, "<pre>#{message}</pre>".html_safe)
+end
