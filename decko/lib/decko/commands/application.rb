545683dec662461c9dd36a6559de022a85cf6e42
Ethan McCutchen <ethan@decko.org>
2022-10-03 13:33:03 -0400

    fix decko command

--- decko/lib/decko/commands/application.rb
+++ decko/lib/decko/commands/application.rb
@@ -2,4 +2,4 @@ require "rails/generators"
 require File.expand_path("../../../generators/deck/deck_generator", __FILE__)
 require "cardio/command"
 
-Cardio::Commands.run_non_deck_command ARGV.first, "decko/commands"
+Cardio::Command.run_non_deck_command ARGV.first, "decko/commands"
