344e59ddbb904497827fc9dfd2544f3e5e06c5a0
Ethan McCutchen <ethan@decko.org>
2022-10-20 11:47:32 -0400

    temporary self set spec fixes

--- card/mod/admin/spec/set/self/admin_info_spec.rb
+++ card/mod/admin/spec/set/self/admin_info_spec.rb
@@ -1,6 +1,11 @@
 # -*- encoding : utf-8 -*-
 
 RSpec.describe Card::Set::Self::AdminInfo do
+  # FIXME: there is no :admin_info card.  Either add it or get rid of this set.
+  def card_subject
+    "A".card.with_set described_class
+  end
+
   specify "view core" do
     expect_view(:core).to have_tag("div.alert.alert-warning.alert-dismissible") do
       with_tag "button.btn-close"
