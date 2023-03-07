2a183ce91d96d61f545df9a03c521ca2a51b6142
Ethan McCutchen <ethan@decko.org>
2023-03-07 14:29:48 -0500

    use standard html5 date editor

--- mod/date/set/all/calendar.rb
+++ mod/date/set/all/calendar.rb
@@ -1,15 +1,15 @@
 basket[:list_input_options] << "calendar"
-basket[:script_calls]["setDatepickerConfig"] = :datepicker_config
+# basket[:script_calls]["setDatepickerConfig"] = :datepicker_config
 
 format :html do
-  def datepicker_config
-    Card::Rule.global_setting :datepicker
-  end
+  # def datepicker_config
+  #   Card::Rule.global_setting :datepicker
+  # end
 
   def calendar_input
-    text_field :content, class: "date-editor datetimepicker-input",
-               "type": "date",
-               "data-bs-toggle": "datetimepicker",
-               "data-bs-target": "##{form_prefix}_content.date-editor"
+    text_field :content, class: "date-editor", # " datetimepicker-input",
+                         type: "date"
+    # "data-bs-toggle": "datetimepicker",
+    # "data-bs-target": "##{form_prefix}_content.date-editor"
   end
 end
