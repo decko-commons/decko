4b7bf1be000ec3747f017c75741af4cc91218b56
Ethan McCutchen <ethan@decko.org>
2023-03-10 16:32:47 -0500

    labeled badge spec fix

--- mod/bootstrap/spec/set/abstract/bs_badge_spec.rb
+++ mod/bootstrap/spec/set/abstract/bs_badge_spec.rb
@@ -2,8 +2,8 @@ RSpec.describe Card::Set::Abstract::BsBadge do
   specify "#labeled_badge" do
     expect(format_subject.labeled_badge(5, "Cats"))
       .to have_tag "span.labeled-badge" do
+      with_tag("label") { "Cats" }
       with_tag("span.badge") { "5" }
-      with_tag("label.text-muted") { "Cats" }
     end
   end
 end
