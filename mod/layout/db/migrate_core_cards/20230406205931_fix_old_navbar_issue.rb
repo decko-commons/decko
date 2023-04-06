e7c75e6abaa929e984e74230e63af59c502c667a
Ethan McCutchen <ethan@decko.org>
2023-04-06 17:08:51 -0400

    layout migration for navbar issue

---
+++ mod/layout/db/migrate_core_cards/20230406205931_fix_old_navbar_issue.rb
@@ -0,0 +1,12 @@
+# -*- encoding : utf-8 -*-
+
+class FixOldNavbarIssue < Cardio::Migration
+  NEST_REGEP = /\{\{\*navbox\|navbar\}\}/
+
+  def up
+    header = "*header".card # name, not codename, is correct here
+    return unless header.content.match? NEST_REGEP
+
+    header.update! content: header.content.gsub(NEST_REGEP, "{{:search|search_box}}")
+  end
+end
