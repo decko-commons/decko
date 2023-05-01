831809fb3e3f6ba27da89a8b44f63632acbeb461
Ethan McCutchen <ethan@decko.org>
2023-05-01 15:28:23 +0200

    cleanup of Abtract::VirtualSearch

--- mod/search/set/right/editors.rb
+++ mod/search/set/right/editors.rb
@@ -1,11 +1,3 @@
-include_set Abstract::VirtualSearch
-
-assign_type :search_type
-
-def cql_content
-  { editor_of: "_left", sort_by: "name" }
-end
-
-def raw_help_text
-  "Users who have edited {{_left|name}}."
-end
+include_set Abstract::VirtualSearch,
+            cql_content: { editor_of: "_left", sort_by: "name" },
+            raw_help_text: "Users who have edited {{_left|name}}."
