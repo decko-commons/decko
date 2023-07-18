56868b4d87b406dc1d3fa5fd319ac479fc3b9762
Ethan McCutchen <ethan@decko.org>
2023-07-18 14:42:01 -0400

    fixed uri spec failure

--- mod/format/spec/set/type/uri_spec.rb
+++ mod/format/spec/set/type/uri_spec.rb
@@ -8,25 +8,28 @@ RSpec.describe Card::Set::Type::Uri do
   end
 
   context "with HTML format" do
+    let(:external_link) { 'a[class="external-link"][href="https://decko.org/Home"]' }
+
     specify "input view" do
       expect_view(:input).to have_tag('input[type="text"][class~="d0-card-content"]')
     end
 
     specify "title_link view" do
-      expect_view(:title_link)
-        .to have_tag('a[class="external-link"][href="https://decko.org/Home"]',
-                     text: "A URI card")
+      expect_view(:title_link).to have_tag(external_link, text: "A URI card")
     end
 
     specify "url_link view" do
-      expect_view(:url_link)
-        .to have_tag('a[class="external-link"][href="https://decko.org/Home"]',
-                     text: "https://decko.org/Home")
+      expect_view(:url_link).to have_tag(external_link, text: "https://decko.org/Home")
+    end
+
+    # calls title_link
+    specify "core view" do
+      expect(format_subject._render_core).to have_tag(external_link, text: "A URI card")
     end
 
     specify "core view with title arg" do
-      expect(format_subject.render_core(title: "My Title"))
-        .to have_tag('a[class="external-link"][href="https://decko.org/Home"]') do
+      expect(format_subject._render_core(title: "My Title"))
+        .to have_tag(external_link) do
           with_tag 'span[class="card-title"]', text: "My Title"
         end
     end
