034e1d42dce4e705254a0783b9fdf2ac1fbd357f
Philipp Kühl <philipp.kuehl@gmail.com>
2023-10-25 01:32:32 +0200

    Add missing bracket

--- mod/script/spec/config/admin_spec.rb
+++ mod/script/spec/config/admin_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe "script mod" do
     aggregate_failures do
       expect(card.settings).to eq %i[script]
       expect(card.configurations).to be_nil
-      expect(card.cardtypes).to eq [["Scripting", %i[java_script coffee_script]], ["Asssets", %i[local_script_folder_group local_script_manifest_group]]
+      expect(card.cardtypes).to eq [["Scripting", %i[java_script coffee_script]], ["Asssets", %i[local_script_folder_group local_script_manifest_group]]]
     end
   end
 end
