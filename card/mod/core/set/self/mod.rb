11ca1ec92b31940c33ca8bc6c323a6190f0ed9cb
Philipp Kühl <philipp.kuehl@gmail.com>
2023-10-26 00:17:23 +0200

    :sunny::sunny::sunny::sunny:

--- card/mod/core/set/self/mod.rb
+++ card/mod/core/set/self/mod.rb
@@ -16,8 +16,8 @@ format :html do
       [
         content_tag(:h1, view_name),
         card.all_admin_configs_grouped_by(:category, :mod)[view_name.to_s]
-          .map do |(mod, configs)|
-          list_section mod.name, configs.map { |c| c.codename.to_sym }
+            .map do |(mod, configs)|
+          list_section(mod.name, configs.map { |c| c.codename.to_sym })
         end.join("<br\>")
       ]
     end
@@ -28,11 +28,11 @@ format :html do
       content_tag(:h1, "Roles"),
       card.all_admin_configs_grouped_by(:roles, :category).map do |(role, configs_by_cat)|
         output [
-                 content_tag(:h2, Card[role.to_sym].name),
-                 (configs_by_cat.map do |(cat, configs)|
-                   list_section cat, configs.map { |c| c.codename }
-                 end)
-               ]
+          content_tag(:h2, Card[role.to_sym].name),
+          (configs_by_cat.map do |(cat, configs)|
+            list_section cat, configs.map(&:codename)
+          end)
+        ]
       end.join("<br\>")
     ]
   end
