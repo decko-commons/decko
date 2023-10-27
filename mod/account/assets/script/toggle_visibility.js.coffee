67f00d3b6d633d680221d18086543aa2f21e4f68
Christina Schweipert <44006691+cschweipert@users.noreply.github.com>
2023-10-27 14:50:53 -0600

    requested changes

--- mod/account/assets/script/toggle_visibility.js.coffee
+++ mod/account/assets/script/toggle_visibility.js.coffee
@@ -3,10 +3,10 @@ $ ->
     passwordField = $("._pw-input")
     togglePassword = $("._pw-text-area")
 
-    if passwordField.prop("type") is "password"
-      passwordField.prop("type", "text")
+    if passwordField.prop "type" is "password"
+      passwordField.prop "type", "text"
       togglePassword.find("i").text("visibility")
     else
-      passwordField.prop("type", "password")
+      passwordField.prop "type", "password"
       togglePassword.find("i").text("visibility_off")
 
--- mod/account/set/right/password.rb
+++ mod/account/set/right/password.rb
@@ -33,24 +33,6 @@ event :validate_password_length, :validate, on: :save do
   errors.add :password, t(:account_password_length, num_char: min_pw_length)
 end
 
-def check_password_regex char_types, regex_hash, password
-  pw_requirements = char_types.each_with_object([]) do |char_type, pw_requirements|
-    if regex_hash.key?(char_type) && password !~ regex_hash[char_type][0]
-      pw_requirements << regex_hash[char_type][1]
-    end
-  end
-  pw_requirements unless pw_requirements.empty?
-end
-
-def format_error_message err_messages
-  error_message = err_messages.map { |message| t(message) }
-  if error_message.length > 2
-    "#{error_message[0...-1].join(", ")}, and #{error_message[-1]}"
-  else
-    error_message.join(" and ")
-  end
-end
-
 event :validate_password_chars, :validate, on: :save do
   pw_requirements = check_password_regex(
     Cardio.config.account_password_requirements,
@@ -59,7 +41,7 @@ event :validate_password_chars, :validate, on: :save do
   )
   return unless pw_requirements
 
-  error_message = format_error_message(pw_requirements)
+  error_message = consolidated_password_error_message(pw_requirements)
   errors.add :password, t(:account_password_requirements, char_type: error_message)
 end
 
@@ -69,6 +51,28 @@ end
 
 view :raw do
   t :account_encrypted
+end
+
+private
+
+def check_password_regex char_types, regex_hash, password
+  pw_requirements = char_types.each_with_object([]) do |char_type, pw_requirements|
+    if regex_hash.key?(char_type) && password !~ regex_hash[char_type][0]
+      pw_requirements << regex_hash[char_type][1]
+    end
+  end
+  pw_requirements unless pw_requirements.empty?
+end
+
+private
+
+def consolidated_password_error_message err_messages
+  error_message = err_messages.map { |message| t(message) }
+  if error_message.length > 2
+    "#{error_message[0...-1].join(", ")}, and #{error_message[-1]}"
+  else
+    error_message.join(" and ")
+  end
 end
 
 format :html do
