b54c939ee64834a96cc15f51ff40397f8e7358fb
Ethan McCutchen <ethan@decko.org>
2024-10-10 17:17:48 -0400

    deeper use of #read_multi

--- card/lib/card/cache.rb
+++ card/lib/card/cache.rb
@@ -5,6 +5,27 @@ class Card
     def cache
       Card::Cache[Card]
     end
+
+    def seed_cache_ids ids
+      # use ids to look up names
+      names = Lexicon.cache.read_multi(ids.map(&:to_s)).values
+      keys = names.map { |n| n.to_name.key }
+
+      # use keys to look up
+      Card.cache.read_multi(keys).each do |key, card|
+        Lexicon.cache.soft.write "L-#{key}", card.id
+      end
+    end
+
+    def seed_cache_names names
+      keys = names.map { |n| n.to_name.key }
+      cache_keys = []
+      Card.cache.read_multi(keys).values.each do |card|
+        cache_keys << card.id.to_s if card.id.present?
+        cache_keys << Lexicon.name_to_cache_key(card.name)
+      end
+      Lexicon.cache.read_multi cache_keys
+    end
   end
 
   # The {Cache} class manages and integrates {Temporary} and {Persistent}
@@ -66,7 +87,7 @@ class Card
     # @param key [String]
     def fetch key, &block
       unless @soft.exist?(key)
-        # Rails.logger.info "FETCH (#{@klass}): #{key}"
+        Rails.logger.info "FETCH (#{@klass}): #{key}"
         tally :fetch
       end
 
--- mod/format/set/all/html/head.rb
+++ mod/format/set/all/html/head.rb
@@ -1,6 +1,6 @@
 basket[:head_views] =
   %i[page_title_tag meta_tags favicon_tag
-     head_remote_stylesheets head_stylesheet
+     stylesheet_tags
      universal_edit_button rss_links]
 # TODO: the last two should be in mods
 
@@ -27,6 +27,7 @@ format :html do
     nest :favicon, view: :link_tag
   end
 
+  # TODO: move to mod
   view :universal_edit_button, unknown: :blank, denial: :blank, perms: :update do
     return if card.new?
 
@@ -34,61 +35,28 @@ format :html do
                 title: "Edit this page!", href: path(view: :edit)
   end
 
-  # these should render a view of the rule card
-  # it would then be safe to cache if combined with param handling
-  # (but note that machine clearing would need to reset card cache...)
-  view :head_stylesheet, unknown: true, cache: :never, perms: :none do
-    return unless (href = head_stylesheet_path)
-
-    tag("link", href: href, media: "all", rel: "stylesheet", type: "text/css")
-  end
-
-  view :head_remote_stylesheets, unknown: true, cache: :never, perms: :none do
-    remote_style_tags
-  end
-
-  def param_or_rule_card setting
-    if params[setting]
-      Card[params[setting]]
-    else
-      root.card.rule_card setting
-    end
-  end
-
-  def debug_or_machine_path setting, debug_lambda, machine_path_lambda
-    return unless (asset_card = param_or_rule_card setting)
-    debug_path(setting, asset_card, &debug_lambda) ||
-      machine_path_lambda.call(asset_card.asset_output_url)
-  end
-
-  def debug_path setting, asset_card
-    return unless params[:debug] == setting.to_s
-
-    yield asset_card
-  end
-
   # TODO: move to rss mod
   view :rss_links, unknown: true, perms: :none do
     render :rss_link_tag if rss_link?
   end
 
-  def rss_link?
-    Card.config.rss_enabled && respond_to?(:rss_link_tag)
-  end
-
-  def remote_style_tags
-    return unless (asset_card = Card[:style_mods])
-
-    asset_card.item_cards.map do |mod_style_card|
-      nest mod_style_card, view: :remote_include_tags
-    end.select(&:present?)
+  # these should render a view of the rule card
+  # it would then be safe to cache if combined with param handling
+  # (but note that machine clearing would need to reset card cache...)
+  view :stylesheet_tags, unknown: true, perms: :none do
+    [nest(:style_mods, view: :remote_style_tags), head_stylesheet_path]
   end
 
   def head_stylesheet_path
-    debug_or_machine_path(
-      :style,
-      ->(style_card) { path mark: style_card.name, item: :import, format: :css },
-      ->(machine_path) { machine_path }
-    )
+    href = nest param_or_rule_card(:style), view: :stylesheet_path
+    tag "link", href: href, media: "all", rel: "stylesheet", type: "text/css"
+  end
+
+  def param_or_rule_card setting
+    params[setting]&.card || root.card.rule_card(setting)
+  end
+
+  def rss_link?
+    Card.config.rss_enabled && respond_to?(:rss_link_tag)
   end
 end
