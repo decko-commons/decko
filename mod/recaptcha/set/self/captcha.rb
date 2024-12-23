d451778c07a06c811a3aa3a4fe1bff7f318826a3
Philipp Kühl <philipp.kuehl@gmail.com>
2024-12-23 17:54:34 +0100

    :sunny:

--- mod/recaptcha/set/self/captcha.rb
+++ mod/recaptcha/set/self/captcha.rb
@@ -1,8 +1,8 @@
 setting_opts group: :permission,
              position: 5,
-             help_text: "Anti-spam setting.  Requires non-signed-in users to complete a " \
-                        "[[http://decko.org/captcha|captcha]] before adding or editing " \
-                        "cards (where permitted)."
+             help_text: "Anti-spam setting.  Requires non-signed-in users to complete " \
+                        "a [[http://decko.org/captcha|captcha]] before adding or " \
+                        "editing cards (where permitted)."
 
 def captcha_used?
   !@captcha_used.nil?
