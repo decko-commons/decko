c5232e32042080de62b66feece3cc3149374b40e
Ethan McCutchen <ethan@decko.org>
2025-01-27 15:52:31 -0500

    html message handling to wikirate

--- mod/email/set/right/html_message.rb
+++ mod/email/set/right/html_message.rb
@@ -4,20 +4,6 @@ def clean_html?
   false
 end
 
-format :html do
-  wrapper :styled_email do
-    haml :styled, body: interior
-  end
-
-  view :core, wrap: :styled_email do
-    super()
-  end
-
-  view :email_css do
-    ""
-  end
-end
-
 format :email_html do
   def email_content context
     content = contextual_content context
