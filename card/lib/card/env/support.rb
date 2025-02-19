9fdb56fa28e0a5ee928bd816a7295e7687318e26
Ethan McCutchen <ethan@decko.org>
2025-02-19 14:38:02 -0500

    anonymous cookies

--- card/lib/card/env/support.rb
+++ card/lib/card/env/support.rb
@@ -21,9 +21,28 @@ class Card
       def reset_session
         if session.is_a? Hash
           @session = {}
-        else
-          controller&.reset_session
+        elsif request
+          update_cookie_setting false
+          controller.reset_session
+          destroy_cookie unless Cardio.config.anonymous_cookies
         end
+      end
+
+      def update_cookie_setting is_anonymous=true
+        return unless request && !Cardio.config.anonymous_cookies
+
+        request.session_options[:drop] = is_anonymous
+      end
+
+      private
+
+      # FIXME: this was breaking signout
+      def destroy_cookie
+        app = Cardio.application
+        app_name = app.class.name ? app.railtie_name.chomp("_application") : ""
+        expire_in_past = "expires=Thu, 01 Jan 1970 00:00:00 GMT;"
+        controller.response.set_header "SET-COOKIE",
+                                       "_#{app_name}_session=deleted; #{expire_in_past}"
       end
     end
   end
