1366952de76747b4672e6311a6a6647a48be2096
Ethan McCutchen <ethan@decko.org>
2025-04-11 20:50:06 -0400

    only call delist on real cards

--- mod/list/set/abstract/delist.rb
+++ mod/list/set/abstract/delist.rb
@@ -1,7 +1,7 @@
 # remove set members from all lists that reference them upon deletion
 event :delist, :prepare_to_store, on: :delete do
   referers.each do |referer|
-    next unless referer.delistable? && referer.is_a?(Abstract::List)
+    next unless referer.delistable? && referer.is_a?(Abstract::List) && referer.real?
 
     referer.drop_item name
     subcard referer
