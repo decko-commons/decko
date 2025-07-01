ceaedbce355a9c8028dbe1fa1c44ecaa9713c74b
Ethan McCutchen <ethan@decko.org>
2025-07-01 14:46:59 -0400

    fix specs affected by new js asset handling

--- mod/assets/spec/set/abstract/asset_outputter_spec.rb
+++ mod/assets/spec/set/abstract/asset_outputter_spec.rb
@@ -3,17 +3,17 @@
 RSpec.describe Card::Set::Abstract::AssetOutputter do
   describe "#make_asset_output_coded" do
     it "creates coded file" do
-      mod_path = Cardio::Mod.dirs.path "format"
+      mod_path = Cardio::Mod.dirs.path "defaults"
       path = File.join mod_path,
-                       "data", "files", "mod_format", "script", "asset_output", "file.js"
+                       "data", "files", "all", "script", "asset_output", "file.js"
       expect(File).to be_exist(path),
                       "Decko should be shipped with generated script file. " \
                       "Couldn't find #{path}"
 
       File.delete path
-      Card[:mod_format, :script, :asset_output].delete
+      Card[:all, :script, :asset_output].delete
 
-      card = Card[:mod_format, :script]
+      card = Card[:all, :script]
       card.update_asset_output
       card.make_asset_output_coded
       expect(File).to be_exist(path)
