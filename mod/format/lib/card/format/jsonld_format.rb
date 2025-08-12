cfc0a135b281bae495f1d64986b5d107e3ffce3c
Vasiliki Gkatziaki <vasgat@gmail.com>
2025-08-12 11:36:42 +0200

    refactor jsonld format

--- mod/format/lib/card/format/jsonld_format.rb
+++ mod/format/lib/card/format/jsonld_format.rb
@@ -1,9 +1,9 @@
 class Card
   class Format
     # card format class for json (JavaScript Object Notation) views
-    class JsonLdFormat < JsonFormat
-      Mime::Type.register "application/ld+json", :json_ld
-      register :json_ld
+    class JsonldFormat < JsonFormat
+      Mime::Type.register "application/ld+json", :jsonld
+      register :jsonld
 
 
       def mime_type
