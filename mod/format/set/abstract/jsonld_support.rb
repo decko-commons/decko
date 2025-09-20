c2e71f2d7b4f2e1cb944e9083972bb96e60b27af
vasgat <vasgat@gmail.com>
2025-09-20 11:27:13 +0200

    refactor(jsonld): encapsulate JSON-LD support in Abstract::JsonldSupported

--- mod/format/set/abstract/jsonld_support.rb
+++ mod/format/set/abstract/jsonld_support.rb
@@ -1,7 +1,10 @@
-format :jsonld do
-  def jsonld_supported? = true
-end
+module Card::Set::Abstract::JsonldSupported
+  extend Card::Set
+    format :jsonld do
+      def jsonld_supported? = true
+    end
 
-def export_formats
-  %i[csv json jsonld]
+    def export_formats
+      %i[csv json jsonld]
+    end
 end
