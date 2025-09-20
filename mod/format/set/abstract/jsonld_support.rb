4c6c8ebf6319628067d0542a133552709b60f09f
vasgat <vasgat@gmail.com>
2025-09-20 12:38:44 +0200

    refactor(jsonld): centralize more helpers

--- mod/format/set/abstract/jsonld_support.rb
+++ mod/format/set/abstract/jsonld_support.rb
@@ -17,6 +17,23 @@ module Card::Set::Abstract::JsonldSupported
       def context
         "#{request.base_url}/context/#{card.type}.jsonld"
       end
+
+      def get_value(metric)
+        metric.value_type == "Multi-Category" ? card.value&.split(", ") : card.value
+      end
+
+      def get_sources
+          sources = card.source_card&.item_names
+          return unless sources.present?
+          sources.map { |name| path(mark: name, format: nil) }
+      end
+
+      def get_unit metric
+          if metric.metric_type == "Relation" || metric.metric_type == "Inverse Relation"
+              return "related companies"
+          end
+          metric.unit.presence
+      end
     end
 
     def export_formats
