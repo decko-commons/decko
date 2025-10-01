ec507d20f16ab7ca6107cd70dcbc4f6849f231f1
Philipp Kühl <philipp.kuehl@gmail.com>
2025-10-01 22:15:25 +0200

    Clean up yml files

--- card/lib/card/set/format/haml_paths.rb
+++ card/lib/card/set/format/haml_paths.rb
@@ -63,8 +63,8 @@ class Card
           path if ::File.exist? path
         end
 
-        def haml_block_locals &block
-          instance_exec(&block) if block_given?
+        def haml_block_locals(&)
+          instance_exec(&) if block_given?
           instance_variables.each_with_object({}) do |var, h|
             h[var.to_s.tr("@", "").to_sym] = instance_variable_get var
           end
